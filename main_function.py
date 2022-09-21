a248bdc256928c57ab25232c782f375bf97e1601
M.R.A <reza.askary99@gmail.com>
2022-09-21 14:29:39 -0500

    Dual_Ascent

--- main_function.py
+++ main_function.py
@@ -22,34 +22,41 @@ class Convex_problems:
         :param x:       R mx1
         :return:
         """
+        ## loss function for minimization
         P = np.eye(self.L)
         F = x.T@P@x
-        L = F +
+        ## linear constraints
+        R = self.A@self.x - self.b  # =0
+        ## The Lagrangian for problem
+        L = F + self.y.T@R
+        return L
 
 
 
     def Dual_Ascent(self, A: np.ndarray = np.eye(1), b: np.ndarray = np.eye(1), alpha :np.float=0.1):
+
         """
         minimize f(x)
         subject to Ax=b
         with variable x ∈ Rn, where A ∈ Rm×n and f : Rn → R is convex
         The Lagrangian for the problem is L(x,y) = f(x) + yT (Ax − b)
         and the dual function is g(y) = inf x L(x,y) = −f ∗(−AT y) − bT y
+        :param A: the matrix of constraint ∈ R(mxn) m linear equation and n adjustable parameters
+        :param b:
+        :param alpha: the learning rate
         :return:
         """
-        n1,m = A.shape
-        n2,p = b.shape
-        if n1 < m:
-            raise Exception('The number of parameters is higher than observations!')
-        if n1!=n2:
-            raise Exception('the input and the output should have the same number of observations!')
-        if p!=1:
+
+        m,n = A.shape
+        m2,n2 = b.shape
+        if m != m2:
+            raise Exception('The number of parameters and equation is not consistent!')
+        if n2 != 1:
             raise Exception('Currently the algorithms is not suitable for multi-output problems!')
-        if self.L != m:
-            raise Exception('The number of adjustable parameters and the dimention of the data is not compatible!')
+
 
         x = np.random.randn(self.L,1)
-        y =  np.random.randn(1,self.L)
+        y =  np.random.randn(1,n1)
         self.y = y
         self.x = y
         self.A = A
