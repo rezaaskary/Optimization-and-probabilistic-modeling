3498e7138d09a3da5f1c70dd7fa31f5a4d308272
M.R.A <reza.askary99@gmail.com>
2022-09-23 08:19:12 -0500

    Dual_Ascent

--- main_function.py
+++ main_function.py
@@ -27,6 +27,11 @@ class Convex_problems:
         return L.ravel()
     #=================================================================
     def dual_ascent_problem(self):
+        """
+        Calculating the partial derivativs with respect to the variables and the lagrange multiplier
+        :return:
+        """
+
         self.dL_dx = np.zeros((self.L,1))
         self.dL_dy = np.zeros((self.m, 1))
         self.h = 1e-12
@@ -37,15 +42,15 @@ class Convex_problems:
                 x_r,x_l = self.x.copy(),self.x.copy()
                 x_r[i] += self.h
                 x_l[i] -= self.h
-                self.dL_dx[i,0] = (1/(2*self.h))*(self.lagrangian(self.A,x_r,self.b,self.y) - self.lagrangian(self.A,x_l,self.b,self.y))
+                self.dL_dx[i,0] = (1/(2*self.h))*(self.lagrangian(x_r,self.y) - self.lagrangian(x_l,self.y))
             # calcualting the derivatives with respect to y
             for i in range(self.m):
                 y_r, y_l = self.y.copy(), self.y.copy()
                 y_r[i] += self.h
                 y_l[i] -= self.h
-                self.dL_dy[i,0] = (1/(2*self.h))*(self.lagrangian(self.A,self.x,self.b,y_r) - self.lagrangian(self.A,self.x,self.b,y_l))
+                self.dL_dy[i,0] = (1/(2*self.h))*(self.lagrangian(self.x, y_r) - self.lagrangian(self.x, y_l))
 
-        elif self.derivatives_method == 'quartic ':     # 4th oder numerical derivatives
+        elif self.derivatives_method == 'quartic':     # 4th oder numerical derivatives
             # calcualting the derivatives with respect to x
             for i in range(self.L):
                 x_rr,x_ll,x_r,x_l = self.x.copy(),self.x.copy(),self.x.copy(),self.x.copy()
@@ -53,8 +58,8 @@ class Convex_problems:
                 x_r[i] += self.h
                 x_ll[i] -= 2*self.h
                 x_l[i] -= self.h
-                self.dL_dx[i,0] = (1/(12*self.h))*(-self.lagrangian(self.A,x_rr,self.b,self.y) + 8.0*self.lagrangian(self.A,x_r,self.b,self.y)\
-                                         - 8.0*self.lagrangian(self.A,x_l,self.b,self.y) + self.lagrangian(self.A,x_ll,self.b,self.y))
+                self.dL_dx[i,0] = (1/(12*self.h))*(-self.lagrangian(x_rr, self.y) + 8.0*self.lagrangian(x_r, self.y)\
+                                         - 8.0*self.lagrangian(x_l, self.y) + self.lagrangian(x_ll, self.y))
             # calcualting the derivatives with respect to y
             for i in range(self.m):
                 y_rr, y_ll, y_r, y_l = self.y.copy(), self.y.copy(), self.y.copy(), self.y.copy()
@@ -62,12 +67,12 @@ class Convex_problems:
                 y_r[i] += self.h
                 y_ll[i] -= 2 * self.h
                 y_l[i] -= self.h
-                self.dL_dy[i, 0] = (1 / (12 * self.h)) * (-self.lagrangian(self.A, self.x, self.b, y_rr) + 8.0 * self.lagrangian(self.A, self.x, self.b,y_r) \
-                            - 8.0 * self.lagrangian(self.A, self.x, self.b, y_l) + self.lagrangian(self.A, self.x, self.b,y_ll))
+                self.dL_dy[i, 0] = (1 / (12 * self.h)) * (-self.lagrangian(self.x, y_rr) + 8.0 * self.lagrangian(self.x, y_r) \
+                            - 8.0 * self.lagrangian(self.x, y_l) + self.lagrangian(self.x, y_ll))
         else:
             raise Exception('Select a proper numerical method for the calculation of the first derivatives!')
-        self.Lag = self.lagrangian(self.A,self.x,self.b,self.y)
-        self.opt = self.loss_f(self,x,A,b,y)
+        self.Lag = self.lagrangian(self.x,self.y)
+        self.opt = self.loss_f(self.x)
 
     #===========================================================================
     def Dual_Ascent(self, A: np.ndarray = np.eye(1), b: np.ndarray = np.eye(1), alpha :float=0.1, tolerance: float=1e-6):
@@ -77,13 +82,15 @@ class Convex_problems:
         with variable x ∈ Rn, where A ∈ Rm×n and f : Rn → R is convex
         The Lagrangian for the problem is L(x,y) = f(x) + yT (Ax − b)
         and the dual function is g(y) = inf x L(x,y) = −f ∗(−AT y) − bT y
-        :param A: the matrix of constraint ∈ R(mxn) m linear equation and n adjustable parameters
+        :param A:
         :param b:
-        :param alpha: the learning rate
+        :param alpha:
+        :param tolerance:
         :return:
         """
+
         self.tolerance = tolerance
-        self.alpha = 0.2;
+        self.alpha = 0.1;
         m,n = A.shape       # m is the number of linear constraints
         m2,n2 = b.shape
 
@@ -105,19 +112,19 @@ class Convex_problems:
         self.A = A
         self.b = b
         self.m = m
-        self.iterations = 2000
+        self.iterations = 5000
         self.derivatives_method = 'quadratic'
+        self.derivatives_method = 'quartic'
 
         for itr in tqdm(range(self.iterations)):
             self.dual_ascent_problem()
-            # print(self.x.T@self.P@self.x)
             self.x = self.x - self.alpha*self.dL_dx
             self.y = self.y + self.alpha*(self.A@self.x-self.b)
 
-
-            # print(f'norm = :  {((self.A@self.x-self.b)**2).sum()}')
                 # print(f'lagrangian:  {L}')
-        self.x
+        print(f'norm = :  {((self.A @ self.x - self.b) ** 2).sum()}')
+        print(self.opt)
+
 
 
 
@@ -126,8 +133,8 @@ class Convex_problems:
 
 
 if __name__=='__main__':
-    A = np.random.rand(4,4)
-    b = np.random.rand(4,1)
+    A = np.random.rand(3,4)
+    b = np.random.rand(3,1)
     D = Convex_problems(problem_type = 1, L= 4)
     D.Dual_Ascent(A=A,b=b,alpha=0.01)
 
