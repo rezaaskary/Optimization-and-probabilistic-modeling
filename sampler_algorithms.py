ba0dbcbada5f3ec8c0c7cf4be1bdb4acdf9f8ec8
M.R.A <reza.askary99@gmail.com>
2022-09-30 13:44:19 -0500

    the multivariable gaussian is added

--- sampler_algorithms.py
+++ sampler_algorithms.py
@@ -63,24 +63,33 @@ def gaussian_liklihood_multivariable_vectorized(measured:np.ndarray, estimated:n
     :param K: The dimention of the multivariable gaussian distribution
     :return: The log liklihood of the multivariable gaussian distribution
     """
+    diagonal_indexes = np.arange(K, dtype=int)
+    diagonal_indexes_samples = np.arange(N, dtype=int)
+    inv_cov = Covariance.copy()   # predefining the inversion matrices
+    Error = measured - estimated  # KxNxC error matrix
+    Error_T = np.transpose(Error, axes=(1, 0, 2))  # NxKxC error matrix
+
     if Diagonal:
-        diagonal_indexes = np.arange(K,dtype=int)
-        diagonal_indexes_samples = np.arange(N,dtype=int)
-        inv_cov = Covariance    # calcualting the inversion of the covariance matrix
+            # calcualting the inversion of the covariance matrix
         inv_cov[diagonal_indexes, diagonal_indexes, :] = 1 / inv_cov[diagonal_indexes, diagonal_indexes,:]  #KxKxX tensor
         det_cov = np.prod(Covariance[diagonal_indexes, Covariance,:],axis = 0)      # 1xC array
-        Error = measured - estimated  # KxNxC error matrix
-        Error_T = np.transpose(Error,axes=(1,0,2))  # NxKxC error matrix
+
         vectorized_mahalanobis_distance = (((Error_T[:, :, None] * inv_cov).sum(axis = 1))[:, :, None] * T).sum(axis = 1) # NxNxC
         mahalanobis_distance = vectorized_mahalanobis_distance[diagonal_indexes_samples, diagonal_indexes_samples, :].sum(axis = 0)
+        log_liklihood = -N * np.log(np.sqrt(((2 * np.pi) ** K) * det_cov)) - 0.5 * mahalanobis_distance
+        return log_liklihood
 
-        log_liklihood = -N * np.log(np.sqrt(((2 * np.pi) ** K) * det_cov))
+    elif not Diagonal:
+        det_cov = np.zeros((1,C))
+        for c in range(C):
+            det_cov[0, c] = np.linalg.det(Covariance[:, :, c])
+            inv_cov[:, :, c:c+1] = np.linalg.inv(Covariance[:, :, c])
 
-        log_liklihood_gaussian = -N * np.log(np.sqrt(((2 * np.pi) ** K) * det_cov)) - (
-                    0.5 * (np.diag(Error.T @ inv_cov @ Error))).sum()
+        vectorized_mahalanobis_distance = (((Error_T[:, :, None] * inv_cov).sum(axis=1))[:, :, None] * T).sum(axis=1)  # NxNxC
 
-
-    return
+    else:
+        raise Exception('The type of calculating the liklihood function is not correctly specified!')
+    return log_liklihood
 
 
 def gaussian_lihlihood_vectorized(parameter, n:int = 1) -> np.ndarray:
