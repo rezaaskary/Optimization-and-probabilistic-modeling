33a758b385f6621161c91af34ec99792d7ac2376
mra <reza.askary99@gmail.com>
2022-10-08 23:18:58 -0500

    probablity distributions

--- sampler_algorithms.py
+++ sampler_algorithms.py
@@ -5,33 +5,36 @@ import matplotlib.pyplot as plt
 from Probablity_distributions import *
 
 
-class Metropolis_Hastings:
-    def __init__(self,logprop_fcn, iterations:int = None, x0:np.ndarray = None, vectorized:bool = False, chains:int = 1, progress_bar:bool = True):
+class MetropolisHastings:
+    def __init__(self, logprop_fcn, iterations: int = None, x_init: np.ndarray = None, vectorized: bool = False,
+                 chains: int = 1, take_derivatives: bool = False, progress_bar: bool = True):
 
-        # checking the correctness of log probablity function
-        if hasattr(logprop_fcn,"__call__"):
+        # checking the correctness of log probability function
+        if hasattr(logprop_fcn, "__call__"):
             self.logprop_fcn = logprop_fcn
         else:
-            raise  Exception('The log(probablity) function is not defined probperly!')
+            raise Exception('The log probability function is not defined properly!')
 
         # checking the correctness of the iteration
         if isinstance(iterations, int):
             self.iterations = iterations
         else:
             self.iterations = 1000
-            print(f'------------------------------------------------------------------------------------------------------------------\n '
-                  f'The iteration is not an integer value. The default value of {self.iterations} is selectd as the number of iterations\n'
-                  f'---------------------------------------------------------------------------------------------------------------------')
+            print(f'-------------------------------------------------------------------------------------------------\n'
+                  f'The iteration is not an integer value.\n'
+                  f' The default value of {self.iterations} is selectd as the number of iterations\n'
+                  f'--------------------------------------------------------------------------------------------------')
 
         # checking the correctness of the iteration
         if isinstance(chains, int):
-            self.Nchain = chains
+            self.n_chains = chains
         else:
-            self.Nchain = 1
+            self.n_chains = 1
             print(
-                f'------------------------------------------------------------------------------------------------------------------\n '
-                f'The number of chains is not an integer value. The default value of {self.Nchain} is selectd as the number of chains\n'
-                f'---------------------------------------------------------------------------------------------------------------------')
+                f'---------------------------------------------------------------------------------------------------\n'
+                f'The number of chains is not an integer value.\n'
+                f' The default value of {self.Nchain} is selectd as the number of chains\n'
+                f'----------------------------------------------------------------------------------------------------')
 
         # checking the correctness of the vectorized simulation
         if isinstance(vectorized, bool):
@@ -59,13 +62,13 @@ class Metropolis_Hastings:
                 f'---------------------------------------------------------------------------------------------------------------------')
 
         # checking the correctness of initial condition
-        if isinstance(x0, np.ndarray):
-            dim1, dim2 = x0.shape
+        if isinstance(x_init, np.ndarray):
+            dim1, dim2 = x_init.shape
             if dim2 != self.Nchain:
                 raise Exception('The initial condition is not consistent with the number of chains!')
             else:
                 self.Ndim = dim1
-                self.x0 = x0
+                self.x0 = x_init
         else:
             raise Exception('The initial condition is not selected properly!')
 
@@ -131,10 +134,9 @@ class Metropolis_Hastings:
         """
 
         # generating the uniform distribution to accept/or reject
-        uniform_random_number = np.random.uniform(low = 0.0, high = 1.0, size = (self.Nchain, self.iterations))
+        uniform_random_number = np.random.uniform(low=0.0, high=1.0, size=(self.Nchain, self.iterations))
 
-        for iter in tqdm(range(1, self.iterations), disable = self.progress_bar):  # sampling from the distribution
-
+        for iter in tqdm(range(1, self.iterations), disable=self.progress_bar):  # sampling from the distribution
             # generating the sample for each chain
             self.proposed = self.gaussian_proposed_distribution(self.chains[:, :, iter - 1:iter].copy(), sigma = 0.1)
 
@@ -152,28 +154,42 @@ class Metropolis_Hastings:
                 self.chains[:, ch, iter:iter + 1] = self.chains[:, ch, iter - 1: iter]
                 self.logprop[ch, iter] = self.logprop[ch, iter - 1]
                 self.accept_rate[ch, iter] = self.n_of_accept[ch, 0] / iter
-
-
-
-
-
         return 1
 
 
+class MCMCHammer:
+    def __init__(self, logprop_fcn, iterations: int = None, x0: np.ndarray = None, vectorized:bool = False,
+                 chains: int = 1, progress_bar: bool = True):
+
+        # checking the correctness of log probability function
+        if hasattr(logprop_fcn, "__call__"):
+            self.logprop_fcn = logprop_fcn
+        else:
+            raise Exception('The log(probability) function is not defined properly!')
+
+        # checking the correctness of the iteration
+        if isinstance(iterations, int):
+            self.iterations = iterations
+        else:
+            self.iterations = 1000
+            print(
+                f'--------------------------------------------------------------------------------------------------\n '
+                f'The iteration is not an integer value.\n'
+                f' The default value of {self.iterations} is selectd as the number of iterations\n'
+                f'---------------------------------------------------------------------------------------------------')
+
+        # checking the correctness of the iteration
+        if isinstance(chains, int):
+            self.Nchain = chains
+        else:
+            self.Nchain = 1
+            print(
+                f'------------------------------------------------------------------------------------------------------------------\n '
+                f'The number of chains is not an integer value. The default value of {self.Nchain} is selectd as the number of chains\n'
+                f'---------------------------------------------------------------------------------------------------------------------')
 
 
-
-
-def Gaussian_liklihood(parameter):
-    x = parameter[0:1,0]
-    # x = parameter[1:,0]
-    mean = 0
-    sigma = 2
-    log_gauss = -np.log(sigma * np.sqrt(2 * np.pi)) - ((x - mean) ** 2) / (2 * sigma ** 2)
-    return log_gauss
-
-
- # logprop_fcn,
+# logprop_fcn,
 # logprop_fcn = Gaussian_liklihood,
 
 
