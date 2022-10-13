9823b67679b1e5949f63f95cab004900f9b9b92d
rezaaskary <reza.askary99@gmail.com>
2022-10-13 17:56:29 -0500

    starting using JAX

--- jnx_Probablity_Distribution_Continuous.py
+++ jnx_Probablity_Distribution_Continuous.py
@@ -1,45 +1,52 @@
-import jax.numpy as jnx
+import jax.numpy as jnp
+import matplotlib.pyplot as plt
 from jax import vmap, jit, grad
 import jax
+from jax.lax import switch
+
 RNG = jax.random.PRNGKey(60616)
 
 
 class ContinuousDistributions:
     def __init__(self,
-                 a: jnx.ndarray = None,
-                 b: jnx.ndarray = None,
-                 fixed_chains: bool = False,
+                 lower: jnp.ndarray = None,
+                 upper: jnp.ndarray = None,
+                 true: bool = False,
                  nchains: int = 1) -> None:
 
-        if isinstance(a, (jnx.ndarray, float, int)):
-            self.a = a
-        elif a is None:
+        if isinstance(lower, (jnp.ndarray, float, int)):
+            self.lower = lower
+        elif lower is None:
             self.a = None
         else:
-            raise Exception('The value of a is not specified correctly!')
+            raise Exception('The value of lower is not specified correctly!')
 
-        if isinstance(b, (jnx.ndarray, float, int)):
-            self.b = b
-        elif b is None:
-            self.b = None
+        if isinstance(upper, (jnp.ndarray, float, int)):
+            self.upper = upper
+        elif upper is None:
+            self.upper = None
         else:
-            raise Exception('The value of b is not specified correctly!')
+            raise Exception('The value of upper is not specified correctly!')
 
 
 class Uniform(ContinuousDistributions):
-    def __init__(self, a: float = None, b: float = None) -> None:
+    def __init__(self, lower: float = None, upper: float = None) -> None:
         """
         Continuous uniform distribution
         :param a: The lower limit of uniform distribution
         :param b: The upper limit of uniform distribution
         """
-        super(Uniform, self).__init__(a=a, b=b)
+        super(Uniform, self).__init__(lower=lower, upper=upper)
         # check for the consistency of the input of the probability distribution
-        if not isinstance(self.a, type(self.b)):
+        if not isinstance(self.lower, type(self.upper)):
             raise Exception('The input parameters are not consistent (Uniform Distribution)!')
 
-        if jnx.any(self.a >= self.b):
+        if jnp.any(self.lower >= self.upper):
             raise Exception('The lower limit of the uniform distribution is greater than the upper limit!')
+
+
+
+
 
     @property
     def statistics(self):
@@ -49,5 +56,32 @@ class Uniform(ContinuousDistributions):
         """
         return None
 
+    @jit
+    def pdf(self, x: jnp.ndarray) -> jnp.ndarray:
+        """
+        Parallelized calculating the probability of the Uniform distribution
+        :param x: An numpy array values determining the variable we are calculating its probability distribution (Cx1)
+        :return: The probability (and the derivative) of the occurrence of the given variable (Cx1, Cx1)
+        """
+        return jnp.where((x > self.lower) & (x < self.upper), 1 / (self.upper - self.lower), 0)
+
+        #
+        # if x <= self.a:
+        #     return 0
+        # elif x >= self.b:
+        #     return 0
+        # else:
+        #     return 1 / (self.b - self.a)
 
 
+# x = (jnx.random.uniform(low = -200, high=200, size=10000)).reshape((-1,1))
+# ts = Uniform(a=4,b=7)
+x = jax.random.uniform(key=RNG, minval=-20, maxval=20, shape=(10, 1))
+
+ts = Uniform(a=4, b=10)
+
+# R = (ts.pdf, in_axes=0, out_axes=0)
+mm=ts.pdf(x)
+R2 = DD(x)
+plt.plot(x, R, '*')
+R
