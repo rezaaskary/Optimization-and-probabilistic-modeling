baa44d99b048b488364bda36bb6ea3fe61f18d50
Reza <maskari@hawk.iit.edu>
2022-10-13 01:42:56 -0500

    probablity distributions is being fixed

--- jnx_Probablity_Distribution_Continuous.py
+++ jnx_Probablity_Distribution_Continuous.py
@@ -7,7 +7,25 @@ RNG = jax.random.PRNGKey(60616)
 class ContinuousDistributions:
     def __init__(self,
                  a: jnx.ndarray = None,
-                 b: jnx.ndarray = None) -> None:
+                 b: jnx.ndarray = None,
+                 variant_chains:  bool = False,
+                 chains: int = 1) -> None:
+
+        if isinstance(variant_chains, bool):
+            self.variant_chains = variant_chains
+        else:
+            raise Exception('Please specify whether the number of chains are fixed or not !')
+
+        if isinstance(chains, int):
+            self.chains = chains
+        elif chains is None:
+            self.chains = None
+        else:
+            raise Exception('Please specify the number of chains!')
+
+
+
+
         if isinstance(a, (jnx.ndarray, float, int)):
             self.a = a
         elif a is None:
@@ -31,6 +49,14 @@ class Uniform(ContinuousDistributions):
         :param b: The upper limit of uniform distribution
         """
         super(Uniform, self).__init__(a=a, b=b)
+
+        if not isinstance(self.a,  type(self.b)):
+            raise Exception('The parameters of the Uniform distribution are not consistent!')
+
+        if not self.variant_chains:
+            if isinstance(self.a, int):
+                self.n_chains = 1
+
 
 
 
