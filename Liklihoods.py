3dfabcba6ebb303e1dd908026accfe2319c4a8a4
rezaaskary <reza.askary99@gmail.com>
2022-10-21 15:06:37 -0500

    truncated normal

--- Liklihoods.py
+++ Liklihoods.py
@@ -1,68 +1,50 @@
 import numpy as np
 import jax.numpy as jnp
+
+
 class Liklihood_Functions:
-    def __int__(self, sigma: jnp.ndarray = np.ones((1,1)), covariance: jnp.ndarray = np.ones((1,1)),\
-                K:int = None, measured: np.ndarray = None, estimated: np.ndarray = None,
-                C: int = None, diagonal_covariance:bool = None):
+    def __int__(self, fcn: str = None, sigma: jnp.ndarray = None, covariance: jnp.ndarray = None,
+                variant_chains: bool = False, activate_jit: bool = False, nchains: int = 1,
+                estimated: np.ndarray = None, measured: np.ndarray = None, diagonal_covariance: bool = None,
+                dimention: bool = None):
 
-        """
-        :param function: A string variable indicating the type of liklihood function
-        :param vectorized:
-        :param Covariance: A positive definite square matrix indicating the covariance matrix of the multivariable Normal distribution (KxK)
-        :param K: The dimention of the multivariable gaussian distribution
-        :param measured: The measured variable
-        :param estimated: The estimated variable(or calculated from a model)
-        :param C: An integer indicating the number of Chains
-        :param diagonal_covariance:
-        :param sigma: The standard deviation of the error estimation
-        :return: the liklihood value(s)
-        """
+        # if not isinstance(vectorized, bool):
+        #     self.vectorized = False
+        #     print(
+        #         f'------------------------------------------------------------------------------------------------------------------\n '
+        #         f'The default value of {self.vectorized} is selectd for vectorizing simulations\n'
+        #         f'---------------------------------------------------------------------------------------------------------------------')
+        # else:
+        #     self.vectorized = vectorized
 
-
-
-        if not isinstance(vectorized, bool):
-            self.vectorized = False
+        if not isinstance(nchains, int):
+            self.nchains = 1
             print(
-                f'------------------------------------------------------------------------------------------------------------------\n '
-                f'The default value of {self.vectorized} is selectd for vectorizing simulations\n'
-                f'---------------------------------------------------------------------------------------------------------------------')
+                f'--------------------------------------------------------------------------------------------------\n '
+                f'The default value of {self.nchains} is selectd for the number of chains\n'
+                f'----------------------------------------------------------------------------------------------------')
         else:
-            self.vectorized = vectorized
+            self.C = nchains
 
-
-        if not isinstance(C, int):
-            self.C = 1
-            print(
-                f'------------------------------------------------------------------------------------------------------------------\n '
-                f'The default value of {self.C} is selectd for the number of chains\n'
-                f'---------------------------------------------------------------------------------------------------------------------')
+        if not isinstance(dimention, int):
+            raise Exception('The dimension of the problem is not specified correctly!')
         else:
-            self.C = C
+            self.dimention = dimention
 
-
-        if not isinstance(K, int):
-            raise Exception('The dimention of the problem is not specified correctly!')
-        else:
-            self.K = K
-
-
-        if not isinstance(N, int):
-            raise Exception('The size of the data is not specified correctly!')
-        else:
-            self.N = N
-
+        # if not isinstance(N, int):
+        #     raise Exception('The size of the data is not specified correctly!')
+        # else:
+        #     self.N = N
 
         if not isinstance(diagonal_covariance, bool):
             raise Exception('The type of calculation(vectorizing) is not specified correctly!')
         else:
-            self.Diagonal = diagonal_covariance
+            self.diagonal_covariance = diagonal_covariance
 
-
-        if not isinstance(Covariance, np.ndarray):
+        if not isinstance(covariance, np.ndarray):
             raise Exception('The covariance matrix is not specified correctly!')
         else:
-            self.Covariance = Covariance
-
+            self.covariance = covariance
 
         if not isinstance(sigma, (int, float)):
             raise Exception('The standard deviation is not specified correctly!')
@@ -84,15 +66,15 @@ class Liklihood_Functions:
         else:
             self.estimated = estimated
 
-        if isinstance(function, str):
-            raise Exception('the type of liklihood function is not specified correctly!')
+        if isinstance(fcn, str):
+            self.fcn = fcn
         else:
-            self.function = function
+            raise Exception('the type of likelihood function is not specified correctly!')
 
         if self.function is 'gaussian_single_variable' and not self.vectorized:
             self.liklihood = gaussian_liklihood_single_variable
 
-        elif self.function is 'gaussian_single_variable' and  self.vectorized:
+        elif self.function is 'gaussian_single_variable' and self.vectorized:
 
             self.liklihood = gaussian_liklihood_single_variable_vectorized
 
@@ -105,7 +87,6 @@ class Liklihood_Functions:
         else:
             raise Exception('The type of entered lklihood function is not implemented!')
 
-
     def gaussian_liklihood_single_variable(self) -> np.ndarray:
         """
         The single variable Gausian liklihood function
@@ -116,9 +97,10 @@ class Liklihood_Functions:
         :return: the log_liklihood function
         """
 
-        log_gauss = -self.N*np.log(self.sigma * np.sqrt(2 * np.pi)) - (((self.measured - self.estimated) ** 2) / (2 * self.sigma ** 2)).sum()
+        log_gauss = -self.N * np.log(self.sigma * np.sqrt(2 * np.pi)) - (
+                    ((self.measured - self.estimated) ** 2) / (2 * self.sigma ** 2)).sum()
         return log_gauss
-        #====================================================================================
+        # ====================================================================================
 
     def gaussian_liklihood_single_variable_vectorized(self) -> np.ndarray:
         """
@@ -131,10 +113,10 @@ class Liklihood_Functions:
         :return: A numpy array indicating the log_liklihood function (1xC)
         """
 
-        vectorized_error = ((self.measured - self.estimated)**2).sum(axis = 0)
-        log_gauss = - self.N*np.log(self.sigma * np.sqrt(2 * np.pi)) - (vectorized_error / (2 * self.sigma ** 2))
+        vectorized_error = ((self.measured - self.estimated) ** 2).sum(axis=0)
+        log_gauss = - self.N * np.log(self.sigma * np.sqrt(2 * np.pi)) - (vectorized_error / (2 * self.sigma ** 2))
         return log_gauss
-        #====================================================================================
+        # ====================================================================================
 
     def gaussian_liklihood_multivariable(self) -> np.ndarray:
         """
@@ -148,20 +130,20 @@ class Liklihood_Functions:
         """
 
         if self.Diagonal:
-            indexes = np.arange(self.K, dtype = int)
+            indexes = np.arange(self.K, dtype=int)
             inv_cov = self.Covariance.copy()
             inv_cov[indexes, indexes] = 1 / inv_cov[indexes, indexes]
             det_cov = self.Covariance.diagonal().prod()
         else:
-            inv_cov = np.linalg.inv(self.Covariance)     # calcualting the inversion of the covariance matrix
-            det_cov = np.linalg.det(self.Covariance)     # calcualting the determinent of the covariance matrix
+            inv_cov = np.linalg.inv(self.Covariance)  # calcualting the inversion of the covariance matrix
+            det_cov = np.linalg.det(self.Covariance)  # calcualting the determinent of the covariance matrix
 
-        Error = self.measured - self.estimated                 # KxN error matrix
-        log_liklihood_gaussian = -self.N * np.log(np.sqrt(((2 * np.pi)**self.K) * det_cov)) - (0.5 * (np.diag(Error.T @ inv_cov @ Error))).sum()  # the log_liklihood gaussian
+        Error = self.measured - self.estimated  # KxN error matrix
+        log_liklihood_gaussian = -self.N * np.log(np.sqrt(((2 * np.pi) ** self.K) * det_cov)) - (
+                    0.5 * (np.diag(Error.T @ inv_cov @ Error))).sum()  # the log_liklihood gaussian
         return log_liklihood_gaussian
 
-
-    def gaussian_liklihood_multivariable_vectorized(self)->np.ndarray:
+    def gaussian_liklihood_multivariable_vectorized(self) -> np.ndarray:
         """
         The log liklihood of the Multivariable gaussian distribution used for multivariable fitting (multivariables objective function)
         :param measured: KxNxC measured parameters (K dimentional parameters and N sampling points and C chains)
@@ -173,28 +155,33 @@ class Liklihood_Functions:
         """
         diagonal_indexes = np.arange(self.K, dtype=int)
         diagonal_indexes_samples = np.arange(self.N, dtype=int)
-        inv_cov = self.Covariance.copy()   # predefining the inversion matrices
+        inv_cov = self.Covariance.copy()  # predefining the inversion matrices
         Error = self.measured - self.estimated  # KxNxC error matrix
-        Error_T = np.transpose(Error, axes = (1, 0, 2))  # NxKxC error matrix
+        Error_T = np.transpose(Error, axes=(1, 0, 2))  # NxKxC error matrix
 
         if self.Diagonal:
-                # calcualting the inversion of the covariance matrix
-            inv_cov[diagonal_indexes, diagonal_indexes, :] = 1 / inv_cov[diagonal_indexes, diagonal_indexes,:]  #KxKxX tensor
-            det_cov = np.prod(self.Covariance[diagonal_indexes, Covariance,:], axis = 0)      # 1xC array
+            # calcualting the inversion of the covariance matrix
+            inv_cov[diagonal_indexes, diagonal_indexes, :] = 1 / inv_cov[diagonal_indexes, diagonal_indexes,
+                                                                 :]  # KxKxX tensor
+            det_cov = np.prod(self.Covariance[diagonal_indexes, Covariance, :], axis=0)  # 1xC array
 
-            vectorized_mahalanobis_distance = (((Error_T[:, :, None] * inv_cov).sum(axis = 1))[:, :, None] * T).sum(axis = 1) # NxNxC
-            mahalanobis_distance = vectorized_mahalanobis_distance[diagonal_indexes_samples, diagonal_indexes_samples, :].sum(axis = 0)
+            vectorized_mahalanobis_distance = (((Error_T[:, :, None] * inv_cov).sum(axis=1))[:, :, None] * T).sum(
+                axis=1)  # NxNxC
+            mahalanobis_distance = vectorized_mahalanobis_distance[diagonal_indexes_samples, diagonal_indexes_samples,
+                                   :].sum(axis=0)
             log_liklihood = -self.N * np.log(np.sqrt(((2 * np.pi) ** self.K) * det_cov)) - 0.5 * mahalanobis_distance
             return log_liklihood
 
         elif not Diagonal:
-            det_cov = np.zeros((1,C))
+            det_cov = np.zeros((1, C))
             for c in range(C):
                 det_cov[0, c] = np.linalg.det(self.Covariance[:, :, c])
-                inv_cov[:, :, c:c+1] = np.linalg.inv(self.Covariance[:, :, c])
+                inv_cov[:, :, c:c + 1] = np.linalg.inv(self.Covariance[:, :, c])
 
-            vectorized_mahalanobis_distance = (((Error_T[:, :, None] * inv_cov).sum(axis=1))[:, :, None] * T).sum(axis = 1)  # NxNxC
-            mahalanobis_distance = vectorized_mahalanobis_distance[diagonal_indexes_samples, diagonal_indexes_samples, :].sum(axis = 0)
+            vectorized_mahalanobis_distance = (((Error_T[:, :, None] * inv_cov).sum(axis=1))[:, :, None] * T).sum(
+                axis=1)  # NxNxC
+            mahalanobis_distance = vectorized_mahalanobis_distance[diagonal_indexes_samples, diagonal_indexes_samples,
+                                   :].sum(axis=0)
             log_liklihood = -self.N * np.log(np.sqrt(((2 * np.pi) ** self.K) * det_cov)) - 0.5 * mahalanobis_distance
             return log_liklihood
         else:
