fafbe520efdb6ae32d1e54fee9a80aca1808417d
Reza <maskari@hawk.iit.edu>
2022-10-24 19:51:08 -0500

    defining a test model

---
+++ test_file.py
@@ -0,0 +1,21 @@
+import jax.numpy as jnp
+from jax import jit, vmap, grad, lax, random
+
+key = random.PRNGKey(23)
+
+x_data_1 = jnp.linspace(0, 10, 200)
+x_data_2 = jnp.linspace(-3, 6, 200)
+std_ = 3
+noise = random.normal(key, shape=(200,)) * 3
+y = 2 * x_data_1 - 6 * x_data_2 + noise
+
+
+def model(par):
+    """
+    given an input of the data, the output of the model is returned
+    :param par:
+    :return:
+    """
+    return par[0] * x_data_1 + par[1] * x_data_2
+
+
