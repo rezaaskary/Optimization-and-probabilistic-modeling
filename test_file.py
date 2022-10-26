6d80a69e669dc10d7d3199303127a092993b4548
rezaaskary <reza.askary99@gmail.com>
2022-10-26 09:10:59 -0500

    truncated normal

--- test_file.py
+++ test_file.py
@@ -3,20 +3,23 @@ from jax import jit, vmap, grad, lax, random
 
 key = random.PRNGKey(23)
 
-x_data_1 = jnp.linspace(0, 10, 200)
-x_data_2 = jnp.linspace(-3, 6, 200)
-std_ = 3
-noise = random.normal(key, shape=(200,)) * 3
-y = 2 * x_data_1 - 6 * x_data_2 + noise
+x_data_1 = (jnp.linspace(0, 10, 200)).reshape((-1,1))
+x_data_2 = random.shuffle(key=key,x=jnp.linspace(-3, 6, 200).reshape((-1,1)))
+X_data = jnp.concatenate((x_data_1,x_data_2,jnp.ones((200,1))),axis=1)
+std_ = 1.5
+noise = random.normal(key, shape=(200,1)) * std_
+theta = jnp.array([2,-6,4]).reshape((-1,1))
 
+y = X_data@theta + noise
 
+y
 def model(par):
     """
     given an input of the data, the output of the model is returned
     :param par:
     :return:
     """
-    return par[0] * x_data_1 + par[1] * x_data_2
+    return X_data@par
 
 def log_posteriori_function(par: jnp.ndarray = None, estimations: jnp.ndarray = None):
     """
