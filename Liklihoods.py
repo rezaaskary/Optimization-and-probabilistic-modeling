88e0e18a26a2f4615bc01bcc04d6fa16c2b4dac4
Reza <maskari@hawk.iit.edu>
2022-10-30 16:36:19 -0500

    parallelizing  the model

--- Liklihoods.py
+++ Liklihoods.py
@@ -2,6 +2,7 @@ import numpy as np
 import jax.numpy as jnp
 from jax import lax, jit, vmap, grad
 
+
 class Liklihood_Functions:
     def __int__(self, fcn: str = None, sigma: jnp.ndarray = None, covariance: jnp.ndarray = None,
                 variant_chains: bool = False, activate_jit: bool = False, nchains: int = 1,
@@ -89,34 +90,32 @@ class Liklihood_Functions:
         #     raise Exception('The type of entered lklihood function is not implemented!')
 
 
-
-
-
-
 class Normal:
-     def __init__(self):
-         self.t = 1
-     def liklihood(self, N, estimated: jnp.ndarray, measured: jnp.ndarray, sigma):
-        error =  ((estimated - measured) ** 2).sum(axis=1)
-        return ((sigma * jnp.sqrt(2 * jnp.pi))**(-N)) * jnp.exp((-0.5/sigma**2) * error)
+    def __init__(self):
+        self.t = 1
 
-     def diff_liklihood(self, N, estimated: jnp.ndarray, measured: jnp.ndarray, sigma):
-         error = (estimated - measured)
-         d_l_d_estimated = ((sigma * jnp.sqrt(2 * jnp.pi)) ** (-N)) * (-1/sigma**2) * (error.sum(axis=1)) *\
-         jnp.exp((-0.5/sigma**2)*(error**2).sum(axis=1))
+    def liklihood(self, N, estimated: jnp.ndarray, measured: jnp.ndarray, sigma):
+        error = ((estimated - measured) ** 2).sum(axis=1)
+        return ((sigma * jnp.sqrt(2 * jnp.pi)) ** (-N)) * jnp.exp((-0.5 / sigma ** 2) * error)
 
-         d_l_d_sigma = jnp.exp((-0.5 / sigma ** 2) * error ** 2) * ((sigma * jnp.sqrt(2 * jnp.pi)) ** (-N)) *\
-         ((-N*jnp.sqrt(2 * jnp.pi))/(sigma * jnp.sqrt(2 * jnp.pi)) + (1/sigma**3) * (error**2).sum(axis=1))
-         return d_l_d_estimated, d_l_d_sigma
+    def diff_liklihood(self, N, estimated: jnp.ndarray, measured: jnp.ndarray, sigma):
+        error = (estimated - measured)
+        d_l_d_estimated = ((sigma * jnp.sqrt(2 * jnp.pi)) ** (-N)) * (-1 / sigma ** 2) * (error.sum(axis=1)) * \
+                          jnp.exp((-0.5 / sigma ** 2) * (error ** 2).sum(axis=1))
 
-     def log_liklihood(self, N, estimated: jnp.ndarray, measured: jnp.ndarray, sigma) -> jnp.ndarray:
-        return -N * jnp.log(sigma * jnp.sqrt(2 * jnp.pi)) - (0.5/sigma**2) * ((estimated - measured) ** 2).sum(axis=1)
+        d_l_d_sigma = jnp.exp((-0.5 / sigma ** 2) * error ** 2) * ((sigma * jnp.sqrt(2 * jnp.pi)) ** (-N)) * \
+                      ((-N * jnp.sqrt(2 * jnp.pi)) / (sigma * jnp.sqrt(2 * jnp.pi)) + (1 / sigma ** 3) * (
+                                  error ** 2).sum(axis=1))
+        return d_l_d_estimated, d_l_d_sigma
 
-     def diff_log_liklihood(self, N, estimated: jnp.ndarray, measured: jnp.ndarray, sigma):
+    def log_liklihood(self, N, estimated: jnp.ndarray, measured: jnp.ndarray, sigma) -> jnp.ndarray:
+        return -N * jnp.log(sigma * jnp.sqrt(2 * jnp.pi)) - (0.5 / sigma ** 2) * ((estimated - measured) ** 2).sum(
+            axis=1)
+
+    def diff_log_liklihood(self, N, estimated: jnp.ndarray, measured: jnp.ndarray, sigma):
         dll_des = (-1 / sigma ** 2) * (estimated - measured).sum(axis=1)
-        dll_sigma = (-N / sigma) + (1 / sigma**3) * ((estimated - measured)**2).sum(axis=1)
+        dll_sigma = (-N / sigma) + (1 / sigma ** 3) * ((estimated - measured) ** 2).sum(axis=1)
         return dll_des, dll_sigma
-
 
 
 class MVNormal:
@@ -137,22 +136,26 @@ class MVNormal:
         #     :return: The log liklihood of the multivariable gaussian distribution
         #     """
 
-
-        error = estimated - measured   # kxnxc
+        error = estimated - measured  # kxnxc
         error_t = jnp.transpose(error, axes=(1, 0, 2))  # nxkxc error matrix
+
         def det_inv(covariance):
             return jnp.linalg.det(covariance), jnp.linalg.inv(covariance)
-        det,inversion = vmap(det_inv,in_axes=[2])(covariance)
 
-        det,inversion = jnp.linalg.det(covariance), jnp.linalg.inv(covariance)
+        det, inversion = vmap(det_inv, in_axes=[2])(covariance)
+
+        det, inversion = jnp.linalg.det(covariance), jnp.linalg.inv(covariance)
 
         def over_samples(xt, inv_cov, x):
             def over_chains(zt, inv_cov, z):
                 return zt @ inv_cov @ z
+
             return vmap(over_chains, in_axes=[-1, None, -1])(xt, inv_cov, x)
+
         distance_points = vmap(over_samples, in_axes=[0, None, 1])(error_t, inversion, error)  # over samples
         mahalanobis_distance = distance_points.sum(axis=0)
 
-        liklihood = ((2 * jnp.pi) ** (-0.5*self.dim*self.n)) * (det**(-0.5*self.n)) * jnp.exp(-0.5 * mahalanobis_distance)
+        liklihood = ((2 * jnp.pi) ** (-0.5 * self.dim * self.n)) * (det ** (-0.5 * self.n)) * jnp.exp(
+            -0.5 * mahalanobis_distance)
 
         return liklihood
