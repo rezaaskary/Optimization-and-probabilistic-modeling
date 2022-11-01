ee3a7eb03746030b0fc3f49366c75fdfd9182d47
Reza <maskari@hawk.iit.edu>
2022-10-31 22:59:00 -0500

    parallelizing  the model

--- test_file.py
+++ test_file.py
@@ -8,12 +8,15 @@ import matplotlib.pyplot as plt
 key = random.PRNGKey(23)
 x_data_1 = (jnp.linspace(0, 10, 200)).reshape((-1, 1))
 x_data_2 = random.shuffle(key=key, x=jnp.linspace(-3, 6, 200).reshape((-1, 1)))
-X_data = jnp.concatenate((x_data_1, x_data_2, jnp.ones((200, 1))), axis=1)
-std_ = 1.5
+# X_data = jnp.concatenate((x_data_1, x_data_2, jnp.ones((200, 1))), axis=1)
+X_data = jnp.concatenate((x_data_1, jnp.ones((200, 1))), axis=1)
+
+std_ = 0.5
 noise = random.normal(key, shape=(200, 1)) * std_
-theta = jnp.array([2, -6, 4]).reshape((-1, 1))
+theta = jnp.array([2, -6]).reshape((-1, 1))
 y = X_data @ theta + noise
-theta2 = random.normal(key, shape=(3, 45))
+y = jnp.tile(y,(1,4))
+theta2 = random.normal(key, shape=(2, 45))
 
 
 def model(par: jnp.ndarray = None, X: jnp.ndarray = None) -> jnp.ndarray:
@@ -36,7 +39,7 @@ def model_no_input(par: jnp.ndarray = None) -> jnp.ndarray:
     return (par ** 2).sum()
 
 
-D = ModelParallelizer(model=model, activate_jit=False, has_input=True)
+D = ModelParallelizer(model=model, activate_jit=True, has_input=True)
 # values = D.model_evaluate(theta2, X_data)
 # values_der = D.diff_model_evaluate(theta2, X_data)
 #
@@ -47,49 +50,54 @@ D = ModelParallelizer(model=model, activate_jit=False, has_input=True)
 
 from jnx_Probablity_Distribution_Continuous import Uniform
 
-theta1 = Uniform(lower=-10, upper=10)
-theta2 = Uniform(lower=-10, upper=10)
-theta3 = Uniform(lower=-10, upper=10)
+theta1 = Uniform(lower=-10, upper=10, activate_jit=True)
+theta2 = Uniform(lower=-10, upper=10, activate_jit=True)
+theta3 = Uniform(lower=-10, upper=10, activate_jit=True)
 # sigma =  HalfNormal(sigma=4)
 
 
 def liklihood(N, estimated: jnp.ndarray, measured: jnp.ndarray, sigma):
     error = ((estimated - measured) ** 2).sum(axis=1)
     return ((sigma * jnp.sqrt(2 * jnp.pi)) ** (-N)) * jnp.exp((-0.5 / sigma ** 2) * error)
-
 def log_liklihood(N, estimated: jnp.ndarray, measured: jnp.ndarray, sigma) -> jnp.ndarray:
-    return -N * jnp.log(sigma * jnp.sqrt(2 * jnp.pi)) -\
-           (0.5 / sigma ** 2) * ((estimated - measured) ** 2).sum(axis=0)
+    er = (0.5 / sigma ** 2) * ((estimated - measured) ** 2).sum(axis=0)
+    return -N * jnp.log(sigma * jnp.sqrt(2 * jnp.pi)) - er
 
 
 def log_posteriori_function(par: jnp.ndarray = None):
     """
     The log of the posteriori distribution
-    :param estimations:
-    :param par: The matrix of the parmaeters
+    :param par: The matrix of the parameters
     :return:
     """
     estimation = D.model_evaluate(par, X_data)
     log_par1 = theta1.log_pdf(par[0:1, :])
     log_par2 = theta2.log_pdf(par[1:2, :])
-    log_par3 = theta3.log_pdf(par[2:3, :])
-    ll = log_liklihood(N=200, estimated=estimation, measured=y, sigma=2)
-    log_posteriori = log_par3 + log_par2 + log_par1 + ll
-    return log_posteriori
+    # log_par3 = theta3.log_pdf(par[2:3, :])
+    ll = log_liklihood(N=200, estimated=estimation, measured=y, sigma=0.5)
+    return log_par2 + log_par1 + ll
 
 
-nchains = 10
+nchains = 4
 theta_init = random.uniform(key=key, minval=0, maxval=1.0, shape=(len(theta), nchains))
 
 T = MetropolisHastings(log_prop_fcn=log_posteriori_function,
                        iterations=1000, chains=nchains, x_init=theta_init,
-                       progress_bar=True, burnin=0, activate_jit=True, random_seed=21)
+                       progress_bar=True, burnin=0, activate_jit=True, random_seed=65)
 S1,S2 = T.sample()
-S1
 
-plt.plot(S1[0,0,:])
+plt.figure(dpi=100)
+plt.plot(S1[0, 0, :])
 plt.show()
-#
+
+plt.figure(dpi=100)
+plt.plot(S1[1, 0, :])
+plt.show()
+
+# plt.figure(dpi=100)
+# plt.plot(S1[2, 0, :])
+# plt.show()
+
 #
 #
 #
