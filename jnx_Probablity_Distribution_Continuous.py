1809aa47b242e8f66f47ce787cc7a16dcd4a9cb0
Reza <maskari@hawk.iit.edu>
2022-11-17 22:13:59 -0600

    adding methods to model vectorizer

--- jnx_Probablity_Distribution_Continuous.py
+++ jnx_Probablity_Distribution_Continuous.py
@@ -12,6 +12,7 @@ class ContinuousDistributions:
                  b: jnp.ndarray = None,
                  lambd: jnp.ndarray = None,
                  beta: jnp.ndarray = None,
+                 gamma: jnp.ndarray = None,
                  mu: jnp.ndarray = None,
                  nu: jnp.ndarray = None,
                  alpha: jnp.ndarray = None,
@@ -35,6 +36,13 @@ class ContinuousDistributions:
             self.nu = None
         else:
             raise Exception('The value of nu is not specified correctly!')
+
+        if isinstance(gamma, (jnp.ndarray, float, int)):
+            self.gamma = kappa
+        elif gamma is None:
+            self.gamma = None
+        else:
+            raise Exception('The value of gamma is not specified correctly!')
 
         if isinstance(kappa, (jnp.ndarray, float, int)):
             self.kappa = kappa
@@ -1472,7 +1480,7 @@ class HalfStudentT(ContinuousDistributions):
         """
 
         coefficient = 2 * (jnp.exp(scipy.special.gammaln((self.nu + 1) / 2)) /
-                       jnp.exp(scipy.special.gammaln(self.nu / 2))) * \
+                           jnp.exp(scipy.special.gammaln(self.nu / 2))) * \
                       jnp.sqrt(1 / (jnp.pi * self.nu))
         return jnp.where(x > 0, coefficient * (1 + (1 / self.nu) * x ** 2) ** (-(self.nu + 1) / 2), 0)
 
@@ -1559,6 +1567,122 @@ class HalfStudentT(ContinuousDistributions):
                   'skewness': None,
                   'kurtosis': None,
                   'entropy': None
+                  }
+        return values
+
+
+class Cauchy(ContinuousDistributions):
+
+    def __init__(self, gamma: float = None, mu: float = None,
+                 activate_jit: bool = False, random_seed: int = 1) -> None:
+        """
+        ------- distribution
+        :param b:
+        :param mu
+        :param activate_jit:
+        """
+        super(Cauchy, self).__init__(ç=gamma, mu=mu,
+                                     activate_jit=activate_jit, random_seed=random_seed)
+        # check for the consistency of the input of the probability distribution
+
+        if self.b <= 0:
+            raise Exception('Parameter b (for calculating the Laplace distribution) should be positive')
+
+        if self.kappa <= 0:
+            raise Exception('The values of Symmetric parameter should be positive(Asymmetric Laplace distribution)!')
+        if self.b <= 0:
+            raise Exception(
+                'The rate of the change of the exponential term should be positive(Asymmetric Laplace distribution)!')
+
+        ContinuousDistributions.parallelization(self)
+
+    def pdf_(self, x: jnp.ndarray) -> jnp.ndarray:
+        """
+        Parallelized calculating the probability of the -------- distribution
+        :param x: An numpy array values determining the variable we are calculating its probability distribution (Cx1)
+        :return: The probability of the occurrence of the given variable Cx1
+        """
+        return jnp.where(x >= self.mu, 1, 1)
+
+    def diff_pdf_(self, x: jnp.ndarray) -> jnp.ndarray:
+        """
+        The derivatives of -------------- distribution
+        :param x: The input variable (Cx1)
+        :return: The derivatives of the probability of the occurrence of the given variable Cx1
+        """
+        return (self.pdf_(x))[0]
+
+    def log_pdf_(self, x: jnp.ndarray) -> jnp.ndarray:
+        """
+        The log of --------- probability distribution
+        :param x: The input variable (Cx1)
+        :return: The log of the probability of the occurrence of the given variable Cx1
+        """
+
+        return -jnp.log(self.pdf_(x))
+
+    def diff_log_pdf_(self, x: jnp.ndarray) -> jnp.ndarray:
+        """
+        The derivatives of ------------ probability distribution
+        :param x: The input variable (Cx1)
+        :return: The log of the probability of the occurrence of the given variable Cx1
+        """
+        return self.log_pdf_(x)[0]
+
+    def cdf_(self, x: jnp.ndarray) -> jnp.ndarray:
+        """
+        The cumulative --------- probability distribution
+        :param x: The input variable (Cx1)
+        :return: The cumulative probability of the occurrence of the given variable Cx1
+        """
+
+        return jnp.where(x >= self.mu, 1, 1)
+
+    def diff_cdf_(self, x: jnp.ndarray) -> jnp.ndarray:
+        """
+        The derivatives of the cumulative ----- probability distribution
+        :param x: The input variable (Cx1)
+        :return: The derivatives cumulative probability of the occurrence of the given variable Cx1
+        """
+        return (self.cdf_(x))[0]
+
+    def log_cdf_(self, x: jnp.ndarray) -> jnp.ndarray:
+        """
+        The log values of the cumulative ----- probability distribution
+        :param x: The input variable (Cx1)
+        :return: The log values of cumulative probability of the occurrence of the given variable Cx1
+        """
+        return jnp.log(self.cdf_(x))
+
+    def diff_log_cdf_(self, x: jnp.ndarray) -> jnp.ndarray:
+        return (self.log_cdf_(x))[0]
+
+    def sample_(self, size: int = 1) -> jnp.ndarray:
+        """
+        Sampling form the --- distribution
+        :param size:
+        :return:
+        """
+        y = random.uniform(key=self.key, minval=0.0, maxval=1.0, shape=(size, 1))
+
+        def inversion_of_cdf_(y: jnp.ndarray) -> jnp.ndarray:
+            return jnp.where(y <= threshold, 1, 1)
+
+        return vmap(inversion_of_cdf_, in_axes=0, out_axes=0)(y)
+
+    @property
+    def statistics(self):
+        """
+        Statistics calculated for the  ---- distribution function given distribution parameters
+        :return: A dictionary of calculated metrics
+        """
+
+        values = {'median': median_,
+                  'mean': mean_,
+                  'variance': variance_,
+                  'skewness': skewness_,
+                  'kurtosis': kurtosis_,
+                  'entropy': entropy_
                   }
         return values
 
