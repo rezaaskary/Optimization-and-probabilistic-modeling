7c99066a455dd5933ca36e81a0dd9bf424aca565
Reza <maskari@hawk.iit.edu>
2022-11-22 23:14:14 -0600

    continuous distribution

--- Discrete.py
+++ Discrete.py
@@ -9,7 +9,9 @@ class DiscreteDistributions:
     def __init__(self,
                  total_count: int = None,
                  probs: int = None,
-                 random_seed: int = 1) -> None:
+                 random_seed: int = 1,
+                 activate_jit: bool = False,
+                 variant_chains: bool = False) -> None:
 
         if isinstance(random_seed, int):
             self.key = random.PRNGKey(random_seed)
@@ -23,36 +25,47 @@ class DiscreteDistributions:
         else:
             raise Exception('The value of total_count is not specified correctly!')
 
-        if isinstance(probs, int):
+        if isinstance(probs, (int, float)):
             self.probs = probs
         elif probs is None:
             self.probs = None
         else:
             raise Exception('The value of probs is not specified correctly!')
 
+        if isinstance(activate_jit, bool):
+            self.activate_jit = activate_jit
+        else:
+            raise Exception('Please specify the activation of the just-in-time evaluation!')
+
+        if isinstance(variant_chains, bool):
+            self.variant_chains = variant_chains
+        else:
+            raise Exception('Please specify whether the number of chains are fixed or variant during simulation !')
+
+
     def parallelization(self):
         if not self.variant_chains:
             # when the number of parallel evaluation is fixed. Useful for MCMC
             if self.activate_jit:
                 self.pdf = jit(vmap(self.pdf_, in_axes=[1], out_axes=1))
-                self.diff_pdf = jit(vmap(grad(self.diff_pdf_), in_axes=[0], out_axes=0))
-                self.log_pdf = jit(vmap(self.log_pdf_, in_axes=[1], out_axes=1))
-                self.diff_log_pdf = jit(vmap(grad(self.diff_log_pdf_), in_axes=[0], out_axes=0))
-                self.cdf = jit(vmap(self.cdf_, in_axes=[1], out_axes=1))
-                self.log_cdf = jit(vmap(self.log_cdf_, in_axes=[1], out_axes=1))
-                self.diff_cdf = jit(vmap(grad(self.diff_cdf_), in_axes=[0], out_axes=0))
-                self.diff_log_cdf = jit(vmap(grad(self.diff_log_cdf_), in_axes=[0], out_axes=0))
-                self.sample = self.sample_
+                # self.diff_pdf = jit(vmap(grad(self.diff_pdf_), in_axes=[0], out_axes=0))
+                # self.log_pdf = jit(vmap(self.log_pdf_, in_axes=[1], out_axes=1))
+                # self.diff_log_pdf = jit(vmap(grad(self.diff_log_pdf_), in_axes=[0], out_axes=0))
+                # self.cdf = jit(vmap(self.cdf_, in_axes=[1], out_axes=1))
+                # self.log_cdf = jit(vmap(self.log_cdf_, in_axes=[1], out_axes=1))
+                # self.diff_cdf = jit(vmap(grad(self.diff_cdf_), in_axes=[0], out_axes=0))
+                # self.diff_log_cdf = jit(vmap(grad(self.diff_log_cdf_), in_axes=[0], out_axes=0))
+                # self.sample = self.sample_
             else:
                 self.sample = self.sample_
-                self.pdf = vmap(self.pdf_, in_axes=[1], out_axes=1)
-                self.diff_pdf = vmap(grad(self.diff_pdf_), in_axes=[0], out_axes=0)
-                self.log_pdf = vmap(self.log_pdf_, in_axes=[1], out_axes=1)
-                self.diff_log_pdf = vmap(grad(self.diff_log_pdf_), in_axes=[0], out_axes=0)
-                self.cdf = vmap(self.cdf_, in_axes=[1], out_axes=1)
-                self.log_cdf = vmap(self.log_cdf_, in_axes=[1], out_axes=1)
-                self.diff_cdf = vmap(grad(self.diff_cdf_), in_axes=[0], out_axes=0)
-                self.diff_log_cdf = vmap(grad(self.diff_log_cdf_), in_axes=[0], out_axes=0)
+                # self.pdf = vmap(self.pdf_, in_axes=[1], out_axes=1)
+                # self.diff_pdf = vmap(grad(self.diff_pdf_), in_axes=[0], out_axes=0)
+                # self.log_pdf = vmap(self.log_pdf_, in_axes=[1], out_axes=1)
+                # self.diff_log_pdf = vmap(grad(self.diff_log_pdf_), in_axes=[0], out_axes=0)
+                # self.cdf = vmap(self.cdf_, in_axes=[1], out_axes=1)
+                # self.log_cdf = vmap(self.log_cdf_, in_axes=[1], out_axes=1)
+                # self.diff_cdf = vmap(grad(self.diff_cdf_), in_axes=[0], out_axes=0)
+                # self.diff_log_cdf = vmap(grad(self.diff_log_cdf_), in_axes=[0], out_axes=0)
 
         else:
             pass
@@ -70,12 +83,14 @@ class DiscreteDistributions:
             y_m.append(self.pdf(x_m[i]))
         plot(list(x_m.ravel()), y_m)
         grid(which='both')
-        show()
 
 
 class Binomial(DiscreteDistributions):
-    def __init__(self, total_count: int = None, probs: int = None) -> None:
-        super(Binomial, self).__init__(total_count=total_count, probs=probs)
+    def __init__(self, total_count: int = None, probs: int = None,
+                 activate_jit: bool = False, random_seed: int = 1) -> None:
+
+        super(Binomial, self).__init__(total_count=total_count, probs=probs,
+                                       activate_jit=activate_jit, random_seed=random_seed)
 
         # check for the consistency of the input of the probability distribution
         if self.total_count < 0:
@@ -84,7 +99,7 @@ class Binomial(DiscreteDistributions):
         if self.probs > 1 or self.probs < 0:
             raise Exception('The of input parameters probs cannot be outside range [0,1] !')
 
-        ContinuousDistributions.parallelization(self)
+        DiscreteDistributions.parallelization(self)
         self.distance_function = distributions.Binomial(total_count=self.total_count, probs=self.probs)
 
     def pdf_(self, x: jnp.ndarray) -> jnp.ndarray:
@@ -137,10 +152,9 @@ class Binomial(DiscreteDistributions):
     #     return values
 
 
-
 # x = random.uniform(key=random.PRNGKey(7), minval=0.01, maxval=20, shape=(1000, 1), dtype=jnp.float64)
-x  = random.randint(key=random.PRNGKey(7),shape=(1000, 1),minval=0,maxval=10,dtype=int)
+x = random.randint(key=random.PRNGKey(7), shape=(1000, 1), minval=0, maxval=10, dtype=int)
 
 KK = Binomial(total_count=20, probs=0.5)
 KK.pdf(x)
-
+KK
