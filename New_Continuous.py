033c741f51c150806fb7bf8a3d2924056d3c467a
Reza <maskari@hawk.iit.edu>
2022-11-23 00:04:45 -0600

    continuous distribution

--- New_Continuous.py
+++ New_Continuous.py
@@ -2,12 +2,29 @@ from tensorflow_probability.substrates.jax import distributions
 from jax import vmap, jit, grad, random, lax, scipy
 import jax.numpy as jnp
 
+
 class ContinuousDistributions:
     def __init__(self,
+                 lower: jnp.ndarray = None,
+                 upper: jnp.ndarray = None,
                  variant_chains: bool = False,
                  activate_jit: bool = False,
                  n_chains: int = 1,
                  random_seed: int = 1) -> None:
+
+        if isinstance(lower, (jnp.ndarray, float, int)):
+            self.lower = lower
+        elif lower is None:
+            self.lower = None
+        else:
+            raise Exception('The value of lower is not specified correctly!')
+
+        if isinstance(upper, (jnp.ndarray, float, int)):
+            self.upper = upper
+        elif upper is None:
+            self.upper = None
+        else:
+            raise Exception('The value of upper is not specified correctly!')
 
         if isinstance(random_seed, int):
             self.key = random.PRNGKey(random_seed)
@@ -31,22 +48,50 @@ class ContinuousDistributions:
             raise Exception('Please specify the activation of the just-in-time evaluation!')
 
 
-
-
 class ContinuousMethods:
 
     def probablity_distribution(self):
         return
+
     def log_probablity_distribution(self):
         return
+
     def cumulative_distribution(self):
         return
+
     def cumulative_distribution(self):
         return
+
     def log_probablity_distribution(self):
         return
+
     def experimental_fit(self):
         return
+
     def sample_from(self):
         return
 
+
+class Uniform(ContinuousDistributions):
+    def __init__(self, lower: float = None, upper: float = None, activate_jit: bool = False,
+                 random_seed: int = 1) -> None:
+        """
+        In probability theory and statistics, the continuous uniform distribution or rectangular distribution is a
+        family of symmetric probability distributions. The distribution describes an experiment where there is an
+        arbitrary outcome that lies between certain bounds. The bounds are defined by the parameters, lower and upper,
+        which are the minimum and maximum values.
+        [1] Dekking, Michel (2005). A modern introduction to probability and statistics : understanding why and how.
+        London, UK: Springer. pp. 60–61. ISBN 978-1-85233-896-1
+
+        Continuous uniform distribution
+        :param lower: The lower limit of uniform distribution
+        :param upper: The upper limit of uniform distribution
+        """
+        super(Uniform, self).__init__(lower=lower, upper=upper, activate_jit=activate_jit, random_seed=random_seed)
+
+        if not isinstance(self.lower, type(self.upper)):
+            raise Exception('The input parameters are not consistent (Uniform Distribution)!')
+        if jnp.any(self.lower >= self.upper):
+            raise Exception('The lower limit of the uniform distribution is greater than the upper limit!')
+
+        self.distance_function = distributions.Uniform(low=self.lower, high=self.upper, name='Uniform')
