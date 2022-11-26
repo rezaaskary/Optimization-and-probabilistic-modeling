59a5e2e16a3ac2c5ed18f401511f3564d3df3631
Reza <maskari@hawk.iit.edu>
2022-11-26 16:56:04 -0600

    continuous distribution

--- New_Continuous.py
+++ New_Continuous.py
@@ -908,6 +908,51 @@ class HalfStudentT(ContinuousDistributions):
         return jnp.clip(a=x, a_min=self.loc, a_max=jnp.inf)
 
 
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+class Cauchy(ContinuousDistributions):
+    def __init__(self, loc: jnp.ndarray = None, scale: jnp.ndarray = None,
+                 activate_jit: bool = False, random_seed: int = 1, validate_input_range: bool = True,
+                 n_chains: int = 1, in_vec_dim: int = 1, out_vec_dim: int = 1) -> None:
+        """
+        Continuous Cauchy distribution
+        :param  : A ndarray or float indicating ----- of the distribution
+        :param  : A ndarray or float indicating ---- of the distribution
+        :param activate_jit: A boolean variable used to activate/deactivate just-in-time evaluation
+        :param random_seed: An integer used to specify the random seed
+        :param multi_distribution: A boolean variable used to indicate the evaluation of multiple probability
+         distribution with different parameters
+        :param n_chains: An integer used to indicate the number of chains/samples
+        :param in_vec_dim: An integer used to indicate the axis of the input variable x for parallelized calculations
+        :param out_vec_dim: An integer used to indicate the axis of the output variable for exporting the output
+        """
+        # recalling parameter values from the main parent class
+        super(Cauchy, self).__init__(loc=loc, scale=scale, activate_jit=activate_jit, random_seed=random_seed,
+                                     n_chains=n_chains, validate_input_range=validate_input_range,
+                                     in_vec_dim=in_vec_dim, out_vec_dim=out_vec_dim)
+        self.name = 'Cauchy'
+        self.distance_function = distributions.Cauchy(loc=self.loc.tolist(), scale=self.scale.tolist(),
+                                                      validate_args=True,
+                                                      name=self.name)
+        ContinuousDistributions.parallelization(self)
+
+    @property
+    def statistics(self):
+        information = {'mean': self.distance_function.mean(name='mean'),
+                       'entropy': self.distance_function.entropy(name='entropy'),
+                       'mode': self.distance_function.mode(name='mode'),
+                       'first_quantile': self.distance_function.quantile(value=0.25, name='first quantile'),
+                       'median': self.distance_function.quantile(value=0.5, name='median'),
+                       'third_quantile': self.distance_function.quantile(value=0.75, name='third quantile'),
+                       'range': self.distance_function.range(name='range'),
+                       'std': self.distance_function.stddev(name='stddev'),
+                       'var': self.distance_function.variance(name='variance'),
+                       }
+        return information
+
+    def valid_range(self, x: jnp.ndarray) -> jnp.ndarray:
+        return jnp.clip(a=x, a_min=jnp.finfo(float).eps, a_max=1.0 - jnp.finfo(float).eps)
+
+
 ##@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 x = random.uniform(key=random.PRNGKey(7), minval=-10, maxval=20, shape=(1, 1000), dtype=jnp.float64)
 # KK = Uniform(lower=jnp.array([4]), upper=jnp.array([7]), activate_jit=True, random_seed=6,
@@ -955,7 +1000,7 @@ E11
 #         :param out_vec_dim: An integer used to indicate the axis of the output variable for exporting the output
 #         """
 #         # recalling parameter values from the main parent class
-#         super(pdf, self).__init__(lower=lower, upper=upper, activate_jit=activate_jit, random_seed=random_seed,
+#         super(pdf, self).__init__(=, =, activate_jit=activate_jit, random_seed=random_seed,
 #                                   n_chains=n_chains, validate_input_range=validate_input_range,
 #                                   in_vec_dim=in_vec_dim, out_vec_dim=out_vec_dim)
 #         self.name = ''
