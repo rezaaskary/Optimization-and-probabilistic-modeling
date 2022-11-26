513b870f49603c243b58adb36faca7ef2073f8a9
Reza <maskari@hawk.iit.edu>
2022-11-25 23:58:14 -0600

    continuous distribution

--- New_Continuous.py
+++ New_Continuous.py
@@ -701,14 +701,14 @@ E10 = KK.maximum_liklihood_estimation(x=x)
 
 
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-# class PDF(ContinuousDistributions):
-#     def __init__(self, : float = None, : float = None,
+# class pdf(ContinuousDistributions):
+#     def __init__(self, : (float, int, jnp.ndarray) = None, : (float, int, jnp.ndarray) = None,
 #                  activate_jit: bool = False, random_seed: int = 1, multi_distribution: bool = True,
 #                  n_chains: int = 1, in_vec_dim: int = 1, out_vec_dim: int = 1) -> None:
 #         """
-#
-#         :param :
-#         :param :
+#         Continuous  distribution
+#         :param  : A ndarray or float indicating ----- of the distribution
+#         :param  : A ndarray or float indicating ---- of the distribution
 #         :param activate_jit: A boolean variable used to activate/deactivate just-in-time evaluation
 #         :param random_seed: An integer used to specify the random seed
 #         :param multi_distribution: A boolean variable used to indicate the evaluation of multiple probability
@@ -718,24 +718,27 @@ E10 = KK.maximum_liklihood_estimation(x=x)
 #         :param out_vec_dim: An integer used to indicate the axis of the output variable for exporting the output
 #         """
 #         # recalling parameter values from the main parent class
-#         super(PDF, self).__init__( activate_jit=activate_jit, random_seed=random_seed,
-#                                       multi_distribution=multi_distribution, n_chains=n_chains,
-#                                       in_vec_dim=in_vec_dim, out_vec_dim=out_vec_dim)
+#         super(pdf, self).__init__(lower=lower, upper=upper, activate_jit=activate_jit, random_seed=random_seed,
+#                                   multi_distribution=multi_distribution, n_chains=n_chains,
+#                                   in_vec_dim=in_vec_dim, out_vec_dim=out_vec_dim)
 #         self.name = ''
 #         # checking the correctness of the parameters
-#         if not isinstance(self.lower, type(self.upper)):
+#         if not isinstance(self., type(self.)):
 #             raise Exception(f'The input parameters are not consistent ({self.name} distribution)!')
 #
-#         if jnp.any(self.lower >= self.upper):
-#             raise Exception(f'The lower limit of the uniform distribution is greater than the upper limit'
-#                             f' ({self.name} distribution)!')
 #
 #         if self.multi_distribution:  # activating multiple distribution
+#             if not isinstance(self., jnp.ndarray):
+#                 raise Exception(f'Please enter the input parameter in the format of ndarray ({self.__class__}'
+#                                 f' distribution)')
 #
-#             self.distance_function = distributions.(low=self.lower.tolist(), high=self.upper.tolist(),
+#             self.distance_function = distributions.(=self..tolist(), =self..tolist(),
 #                                                            name=self.name)
 #         else:  # activating single distribution
-#             self.distance_function = distributions.(low=self.lower, high=self.upper, name=self.name)
+#             if isinstance(self., jnp.ndarray):
+#                 raise Exception(f'Please enter the input parameter in the format of float ({self.__class__}'
+#                                 f' distribution)')
+#             self.distance_function = distributions.Uniform(=self., =self., name=self.name)
 #
 #         ContinuousDistributions.parallelization(self)
 #
