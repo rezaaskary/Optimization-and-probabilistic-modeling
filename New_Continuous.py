671d2368094e079790257f63a114a0cc468236d2
Reza <maskari@hawk.iit.edu>
2022-11-26 18:23:31 -0600

    continuous distribution

--- New_Continuous.py
+++ New_Continuous.py
@@ -1525,6 +1525,50 @@ class LogitNormal(ContinuousDistributions):
 
     def valid_range(self, x: jnp.ndarray) -> jnp.ndarray:
         return x
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+class TruncatedCauchy(ContinuousDistributions):
+    def __init__(self, loc: jnp.ndarray = None, scale: jnp.ndarray = None, lower: jnp.ndarray = None,
+                 upper: jnp.ndarray = None,
+                 activate_jit: bool = False, random_seed: int = 1, validate_input_range: bool = True,
+                 n_chains: int = 1, in_vec_dim: int = 1, out_vec_dim: int = 1) -> None:
+        """
+        Continuous TruncatedCauchy distribution
+        :param  : A ndarray or float indicating ----- of the distribution
+        :param  : A ndarray or float indicating ---- of the distribution
+        :param activate_jit: A boolean variable used to activate/deactivate just-in-time evaluation
+        :param random_seed: An integer used to specify the random seed
+        :param multi_distribution: A boolean variable used to indicate the evaluation of multiple probability
+         distribution with different parameters
+        :param n_chains: An integer used to indicate the number of chains/samples
+        :param in_vec_dim: An integer used to indicate the axis of the input variable x for parallelized calculations
+        :param out_vec_dim: An integer used to indicate the axis of the output variable for exporting the output
+        """
+        # recalling parameter values from the main parent class
+        super(TruncatedCauchy, self).__init__(loc=loc, scale=scale, lower=lower, upper=upper, activate_jit=activate_jit, random_seed=random_seed,
+                                  n_chains=n_chains, validate_input_range=validate_input_range,
+                                  in_vec_dim=in_vec_dim, out_vec_dim=out_vec_dim)
+        self.name = 'TruncatedCauchy'
+        self.distance_function = distributions.TruncatedCauchy(loc=self.loc.tolist(), scale=self.scale.tolist(),
+                                                               low=self.lower.tolist(), high=self.upper.tolist(),
+                                                               validate_args=True,
+                                                                name=self.name)
+        ContinuousDistributions.parallelization(self)
+
+    @property
+    def statistics(self):
+        information = {'mean': self.distance_function.mean(name='mean'),
+                       'entropy': self.distance_function.entropy(name='entropy'),
+                       'mode': self.distance_function.mode(name='mode'),
+                       'first_quantile': self.distance_function.quantile(value=0.25, name='first quantile'),
+                       'median': self.distance_function.quantile(value=0.5, name='median'),
+                       'third_quantile': self.distance_function.quantile(value=0.75, name='third quantile'),
+                       'range': self.distance_function.range(name='range'),
+                       'std': self.distance_function.stddev(name='stddev'),
+                       'var': self.distance_function.variance(name='variance'),
+                       }
+        return information
+    def valid_range(self, x: jnp.ndarray) -> jnp.ndarray:
+        return jnp.clip(a=x, a_min=jnp.finfo(float).eps, a_max=1.0 - jnp.finfo(float).eps)
 
 
 ##@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
