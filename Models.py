89695fac004bcc8322b0f7279cdf5f911b127f7b
Reza <maskari@hawk.iit.edu>
2022-11-27 11:36:00 -0600

    continuous distribution

--- Models.py
+++ Models.py
@@ -3,6 +3,46 @@ from jax import vmap, jit, grad, random, lax, scipy, jacfwd
 import jax.numpy as jnp
 
 
+def emppca_complete(y, k, w, v, maxiter, tolfun, tolx, dispnum, iterfmtstr):
+    p, n = Y.shape
+    mu = np.mean(Y, axis=1)[:, np.newaxis]
+    Y -= np.tile(mu, [1, n])
+    iter = 0
+    nloglk = np.inf
+    traceS = ((Y.reshape((-1, 1))).T @ Y.reshape((-1, 1))) / (n - 1)
+    eps = np.finfo(float).eps
+    while iter < MaxIter:
+        iter += 1
+        SW = Y @ (Y.T @ W) / (n - 1)
+        M = W.T @ W + v * np.eye(k)
+        Wnew = SW @ np.linalg.inv(v * np.eye(k) + np.linalg.inv(M) @ W.T @ SW)
+        vnew = (traceS - np.trace(SW @ np.linalg.inv(M) @ Wnew.T)) / p
+
+        dw = (np.abs(W - Wnew) / (np.sqrt(eps) + (np.abs(Wnew)).max())).max()
+        dv = np.abs(v - vnew) / (eps + v)
+        delta = max([dw, dv])
+        CC = Wnew @ Wnew.T + vnew * np.eye(p)
+        nloglk_new = (p * np.log(2 * np.pi) + np.log(np.linalg.det(CC)) + \
+                      np.trace(np.linalg.inv(CC) @ Y @ Y.T / (n - 1))) * n / 2
+        W = Wnew
+        v = vnew
+        print(delta)
+        print(np.abs(nloglk - nloglk_new))
+        if delta < TolX:
+            break
+        elif (nloglk - nloglk_new) < TolFun:
+            break
+        elif np.abs(vnew) < np.sqrt(eps):
+            break
+        nloglk = nloglk_new
+    ##=====================================================
+    Xmu = np.linalg.inv(M) @ Wnew.T @ Y
+    return Wnew, Xmu, mu, vnew, iter, dw, nloglk_new
+
+
+#######################################################################################
+
+
 class PPCA:
     def __init__(self, y: jnp.ndarray = None, k: int = 2):
         if not (min(y.shape) > 1): raise Exception('Too few feature variables')
@@ -68,7 +108,7 @@ class PPCA:
                 for j in range(n):
                     y_sample = y[:, j:j + 1]
                     idxobs = obs[:, j]
-                    w_sample = W[idxobs, :]
+                    w_sample = w[idxobs, :]
                     # Use Sherman-Morrison formula to find the inv(v.*eye(k)+w'*w)
                     cj = jnp.eye(k) / v - (w_sample.T @ w_sample) @ np.linalg.inv(
                         np.eye(k) + (w_sample.T @ w_sample) / v) / (v ** 2)
@@ -116,8 +156,8 @@ class PPCA:
             iterfmtstr = ''
             W, X, mu, v, itercount, dw, nloglk = emppca_complete(y, k, w, v, maxiter, tolfun, tolx, dispnum, iterfmtstr)
 
-            if np.all(W == 0):
-                coeff = np.zeros((p, k))
+            if jnp.all(w == 0):
+                coeff = jnp.zeros((p, k))
                 coeff[::(p + 1)] = 1
                 score = np.zeros((n, k))
                 latent = np.zeros(k, 1)
