70b50402b4a5c3d13bc8ec8fbd450ee7d6004de3
Reza <maskari@hawk.iit.edu>
2022-11-26 19:20:31 -0600

    continuous distribution

--- Discrete.py
+++ Discrete.py
@@ -64,6 +64,25 @@ class DiscreteDistributions:
         else:
             raise Exception(f'Please specify the activation of the just-in-time evaluation'
                             f' ({self.__class__} distribution)!')
+
+            # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+            if isinstance(n, jnp.ndarray):
+                self.n = n
+            elif n is None:
+                self.n = None
+            else:
+                raise Exception(
+                    f'The value of input parameters is not specified correctly. Please enter parameters  in the'
+                    f' format of ndarrauy ({self.__class__} distribution)!')
+
+
+
+
+
+
+
+
+
         # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
         if isinstance(lower, jnp.ndarray):
             self.lower = lower
@@ -376,52 +395,46 @@ class DiscreteDistributions:
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
+class Binomial(DiscreteDistributions):
+    def __init__(self, n: jnp.ndarray = None, p: jnp.ndarray = None,
+                 activate_jit: bool = False, random_seed: int = 1, validate_input_range: bool = True,
+                 n_chains: int = 1, in_vec_dim: int = 1, out_vec_dim: int = 1) -> None:
+        """
+        Discrete Binomial distribution
+        :param  : A ndarray or float indicating ----- of the distribution
+        :param  : A ndarray or float indicating ---- of the distribution
+        :param activate_jit: A boolean variable used to activate/deactivate just-in-time evaluation
+        :param random_seed: An integer used to specify the random seed
+        :param multi_distribution: A boolean variable used to indicate the evaluation of multiple probability
+         distribution with different parameters
+        :param n_chains: An integer used to indicate the number of chains/samples
+        :param in_vec_dim: An integer used to indicate the axis of the input variable x for parallelized calculations
+        :param out_vec_dim: An integer used to indicate the axis of the output variable for exporting the output
+        """
+        # recalling parameter values from the main parent class
+        super(Binomial, self).__init__(n=n, p=p, activate_jit=activate_jit, random_seed=random_seed,
+                                  n_chains=n_chains, validate_input_range=validate_input_range,
+                                  in_vec_dim=in_vec_dim, out_vec_dim=out_vec_dim)
+        self.name = ''
+        self.distance_function = distributions.Binomial(=self..tolist(), =self..tolist(),
+                                                           name=self.name)
+        ContinuousDistributions.parallelization(self)
 
-
-
-
-
-
-# class pdf(DiscreteDistributions):
-#     def __init__(self, : jnp.ndarray = None, : jnp.ndarray = None,
-#                  activate_jit: bool = False, random_seed: int = 1, validate_input_range: bool = True,
-#                  n_chains: int = 1, in_vec_dim: int = 1, out_vec_dim: int = 1) -> None:
-#         """
-#         Continuous  distribution
-#         :param  : A ndarray or float indicating ----- of the distribution
-#         :param  : A ndarray or float indicating ---- of the distribution
-#         :param activate_jit: A boolean variable used to activate/deactivate just-in-time evaluation
-#         :param random_seed: An integer used to specify the random seed
-#         :param multi_distribution: A boolean variable used to indicate the evaluation of multiple probability
-#          distribution with different parameters
-#         :param n_chains: An integer used to indicate the number of chains/samples
-#         :param in_vec_dim: An integer used to indicate the axis of the input variable x for parallelized calculations
-#         :param out_vec_dim: An integer used to indicate the axis of the output variable for exporting the output
-#         """
-#         # recalling parameter values from the main parent class
-#         super(pdf, self).__init__(=, =, activate_jit=activate_jit, random_seed=random_seed,
-#                                   n_chains=n_chains, validate_input_range=validate_input_range,
-#                                   in_vec_dim=in_vec_dim, out_vec_dim=out_vec_dim)
-#         self.name = ''
-#         self.distance_function = distributions.(=self..tolist(), =self..tolist(),
-#                                                            name=self.name)
-#         ContinuousDistributions.parallelization(self)
-#
-#     @property
-#     def statistics(self):
-#         information = {'mean': self.distance_function.mean(name='mean'),
-#                        'entropy': self.distance_function.entropy(name='entropy'),
-#                        'mode': self.distance_function.mode(name='mode'),
-#                        'first_quantile': self.distance_function.quantile(value=0.25, name='first quantile'),
-#                        'median': self.distance_function.quantile(value=0.5, name='median'),
-#                        'third_quantile': self.distance_function.quantile(value=0.75, name='third quantile'),
-#                        'range': self.distance_function.range(name='range'),
-#                        'std': self.distance_function.stddev(name='stddev'),
-#                        'var': self.distance_function.variance(name='variance'),
-#                        }
-#         return information
-#     def valid_range(self, x: jnp.ndarray) -> jnp.ndarray:
-#         return jnp.clip(a=x, a_min=jnp.finfo(float).eps, a_max=1.0 - jnp.finfo(float).eps)
+    @property
+    def statistics(self):
+        information = {'mean': self.distance_function.mean(name='mean'),
+                       'entropy': self.distance_function.entropy(name='entropy'),
+                       'mode': self.distance_function.mode(name='mode'),
+                       'first_quantile': self.distance_function.quantile(value=0.25, name='first quantile'),
+                       'median': self.distance_function.quantile(value=0.5, name='median'),
+                       'third_quantile': self.distance_function.quantile(value=0.75, name='third quantile'),
+                       'range': self.distance_function.range(name='range'),
+                       'std': self.distance_function.stddev(name='stddev'),
+                       'var': self.distance_function.variance(name='variance'),
+                       }
+        return information
+    def valid_range(self, x: jnp.ndarray) -> jnp.ndarray:
+        return jnp.clip(a=x, a_min=jnp.finfo(float).eps, a_max=1.0 - jnp.finfo(float).eps)
 
 
 
@@ -431,7 +444,7 @@ class DiscreteDistributions:
 #                  activate_jit: bool = False, random_seed: int = 1, validate_input_range: bool = True,
 #                  n_chains: int = 1, in_vec_dim: int = 1, out_vec_dim: int = 1) -> None:
 #         """
-#         Continuous  distribution
+#         Discrete  distribution
 #         :param  : A ndarray or float indicating ----- of the distribution
 #         :param  : A ndarray or float indicating ---- of the distribution
 #         :param activate_jit: A boolean variable used to activate/deactivate just-in-time evaluation
