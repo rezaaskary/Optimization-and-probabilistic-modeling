521766c5ba652fe58f0c88d63418866626c02388
Reza <maskari@hawk.iit.edu>
2022-11-28 23:36:44 -0600

    continuous distribution

--- Models.py
+++ Models.py
@@ -102,41 +102,10 @@ class PPCA_:
             diff = nloglk - nloglk_new
             return itr, vnew, wnew, nloglk_new, delta, diff
 
-        # TT = (self.itr,
-        #             self.v.copy(),
-        #             self.w.copy(),
-        #             self.nloglk.copy(),
-        #             self.delta.copy(),
-        #             self.diff.copy(),
-        #             )
-        # for i in range(100):
-        #     TT = body_fun(value=TT)
-        def true_fcn():
-            return True
-
-        def false_fcn():
-            return False
-
-        def cond_fun(value: tuple = None) -> bool:
+        def cond_fun(value: tuple = None):
             itr, v, w, nloglk, delta, diff = value
-            return (itr < self.max_iter) | (jnp.abs(diff) > self.tolerance) | (delta > self.tolerance) | (jnp.abs(v) > jnp.sqrt(self.tolerance))
-            # return lax.cond(itr < 100, lambda x: True, lambda x: False)
-            # return lax.cond((itr < self.max_iter) |
-            #                 (jnp.abs(diff) > self.tolerance) |
-            #                 (delta > self.tolerance) |
-            #                 (jnp.abs(v) > jnp.sqrt(self.tolerance))
-            #                 ,
-            #                 true_fcn(),
-            #                 false_fcn(), itr)
-            # return bool(~jnp.logical_or(delta < self.tolerance, diff < self.tolerance, itr < self.max_iter,
-            #                      jnp.abs(v) < jnp.sqrt(self.tolerance)))
-
-            # return jnp.where(delta < self.tolerance, False,
-            #                       jnp.where(jnp.abs(diff) < self.tolerance, False,
-            #                                 jnp.where(jnp.abs(v) < jnp.sqrt(self.tolerance), False,
-            #                                           jnp.where(itr < self.max_iter, False, True))))
-            # return itr < 10
-
+            arg4 = jnp.abs(diff).astype(float)
+            return (arg4 > self.tolerance) | (itr < self.max_iter)
 
         self.itr, \
         self.v, \
@@ -329,5 +298,5 @@ if __name__ == '__main__':
     data = data.at[:, 2].set(jnp.nan)
     data = data.at[3, :].set(jnp.nan)
     # PPCA(y=data,k=2)
-    D = PPCA_(y=data, n_comp=2, max_iter=500, tolerance=1e-6)
+    D = PPCA_(y=data, n_comp=2, max_iter=500, tolerance=1e-5)
     D.run()
--- man_mcmc_simulation.py
+++ man_mcmc_simulation.py
@@ -1,18 +1,31 @@
 import numpy as np
-from jax import random
+from jax import random, lax
+import jax.numpy as jnp
+
 # from Probablity_distributions import *
 from tensorflow_probability.substrates.jax import distributions
 
-x  = random.randint(key=random.PRNGKey(7),shape=(1000, 1),minval=0,maxval=10,dtype=int)
+x = random.randint(key=random.PRNGKey(7), shape=(1000, 1), minval=0, maxval=10, dtype=int)
 
-M = distributions.Binomial(total_count=20,probs=0.5)
+M = distributions.Binomial(total_count=20, probs=0.5)
 
-RR = M.prob([0,0.2,1,2,3,4,5,55])
-
-RR
+RR = M.prob([0, 0.2, 1, 2, 3, 4, 5, 55])
 
 
+def true_fcn(itr):
+    return True
 
+
+def false_fcn(itr):
+    return False
+
+
+itr = jnp.array(4).reshape((1,))
+iir = jnp.array(2).reshape((1,))
+er = jnp.array(2.8).reshape((1,))
+T = lax.cond(pred=((7 < 100)), true_fun=lambda: True, false_fun=lambda: False)
+# T = lax.cond(itr.item() < 100, true_fcn(itr), false_fcn(itr),itr)
+T
 
 
 def vc(x):
@@ -22,13 +35,12 @@ def vc(x):
         raise Exception('The format of the input variable is not supported!')
 
 
-
-
 def log_posteriori_fcn():
     parameter1 = Uniform(a=0, b=4, return_der_pdf=False, return_der_logpdf=False)
     parameter2 = Normal(sigma=3, mu=0, return_der_pdf=False, return_der_logpdf=False)
-    CC=parameter1.pdf(np.array(.1))
+    CC = parameter1.pdf(np.array(.1))
 
     return
+
 
 T = log_posteriori_fcn()
