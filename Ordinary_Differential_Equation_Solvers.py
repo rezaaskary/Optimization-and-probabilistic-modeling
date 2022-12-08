255d9cf03eb5eae4da403b24016b6028262b82bc
rezaaskary <reza.askary99@gmail.com>
2022-12-08 17:59:44 -0600

    sd

--- Ordinary_Differential_Equation_Solvers.py
+++ Ordinary_Differential_Equation_Solvers.py
@@ -4,12 +4,12 @@ import jax
 from tqdm import tqdm
 
 
-def ode_fcn(x: jnp.ndarray = None, p: jnp.ndarray = None, t: jnp.ndarray = None) -> jnp.ndarray:
+def ode_fcn(x: jnp.ndarray = None, p: jnp.ndarray = None, t: jnp.ndarray = None, u: jnp.ndarray = None) -> jnp.ndarray:
     m = 4  # the number of state variables
     dx_dt = jnp.zeros((m, 1))  # reallocating the values of state variables
-    dx0_dt = -6 * x[0] + x[1] * x[2] * p[0]
+    dx0_dt = -6 * x[0] + x[1] * x[2] * p[0] + u[0]
     dx1_dt = -2 * x[1] + jnp.sin(x[3] + x[2])
-    dx2_dt = -8 * x[2] + jnp.sin(x[0] + x[1] * p[2])
+    dx2_dt = -8 * x[2] + jnp.sin(x[0] + x[1] * p[2]) + u[1]
     dx3_dt = -3 * x[3] + jnp.cos(x[3] ** p[3] * x[1])
 
     dx_dt = dx_dt.at[0, 0].set(dx0_dt)
@@ -25,8 +25,9 @@ chains = 10000
 L = 10000
 par = jax.random.uniform(key=jax.random.PRNGKey(7), minval=-4, maxval=4, shape=(n_par, chains), dtype=jnp.float64)
 x_0 = jax.random.uniform(key=jax.random.PRNGKey(7), minval=-4, maxval=4, shape=(4, chains), dtype=jnp.float64)
+u = jax.random.uniform(key=jax.random.PRNGKey(7), minval=-4, maxval=4, shape=(2, L), dtype=jnp.float64)
 
-d_dx = jax.vmap(fun=ode_fcn, in_axes=[1, 1, None], out_axes=1)
+d_dx = jax.vmap(fun=ode_fcn, in_axes=[1, 1, None, None], out_axes=1)
 x = jnp.zeros((4, chains, L))
 x = x.at[:, :, 0].set(x_0)
 delta = 1
@@ -36,15 +37,17 @@ delta = 1
 
 
 def wrapper(itr: int, init_val: tuple) -> tuple:
-    x, par = init_val
-    x=x.at[:, :, itr + 1].set(d_dx(x[:, :, itr], par, itr)[:, :, 0])
-    return x, par
+    x, par, u = init_val
+    evaluation = d_dx(x[:, :, itr], par, itr, u[:, itr])[:, :, 0]
+    # [:, :, 0]
+    x=x.at[:, :, itr + 1].set(evaluation)
+    return x, par, u
 
 
 M = jax.lax.fori_loop(lower=0,
                       upper=L,
                       body_fun=wrapper,
-                      init_val=(x, par))
+                      init_val=(x, par, u))
 M
 
 
