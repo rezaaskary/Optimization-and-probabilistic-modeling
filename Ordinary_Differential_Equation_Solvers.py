f42a8014149bc12105a8d110b6b8ff7913535329
reza <maskari@hawk.iit.edu>
2022-12-08 22:59:38 -0600

    adding Opax

--- Ordinary_Differential_Equation_Solvers.py
+++ Ordinary_Differential_Equation_Solvers.py
@@ -44,14 +44,15 @@ M = jax.lax.fori_loop(lower=0,
                       upper=L,
                       body_fun=wrapper,
                       init_val=(x, par))
-M
-
 
 class ODESolvers:
     def __int__(self,
                 fcn: callable = None,
                 n: int = None,
-                method: str = 'Euler'):
+                n_sim: int = 1,
+                n_states: int = None,
+                method: str = 'Euler',
+                activate_jit: bool = False):
         if isinstance(method, str):
             if method not in ['Euler', 'rk2', 'rk4']:
                 self.method = method
@@ -61,3 +62,10 @@ class ODESolvers:
             self.method = 'Euler'
         else:
             raise Exception(f'Please enter the method for solving system of ordinary differential equation.')
+
+        if isinstance(n_sim, int):
+            self.n_sim = n_sim
+        elif not n_sim:
+            self.n_sim = 1
+        else:
+            raise
