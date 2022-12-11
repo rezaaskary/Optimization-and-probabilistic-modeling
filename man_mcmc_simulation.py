a28338f66f49c558491f9a4c770705a7bd5bb621
reza <maskari@hawk.iit.edu>
2022-12-11 11:46:42 -0600

    adding ode solvers

--- man_mcmc_simulation.py
+++ man_mcmc_simulation.py
@@ -2,18 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 from jax import random, vmap, lax, jacfwd, jit, value_and_grad
 import jax.numpy as jnp
+import jax
 import optax
 from sklearn.decomposition import FactorAnalysis
 import pandas as pd
 from Ordinary_Differential_Equation_Solvers import ODESolvers
 
+
 def ode_fcn(x: jnp.ndarray = None, p: jnp.ndarray = None, t: jnp.ndarray = None, u: jnp.ndarray = None) -> jnp.ndarray:
     m = 4  # the number of state variables
     dx_dt = jnp.zeros((m, 1))  # reallocating the values of state variables
-    dx0_dt = -0.2 * x[0] + x[1] * x[2] + 0.1 * u[2]
-    dx1_dt = -0.1 * x[1] + jnp.sin(x[1] + x[3]) + 0.3 * u[1]
+    dx0_dt = -0.2 * x[0] + p[0] * x[1] * x[2] + 0.1 * u[2]
+    dx1_dt = -0.1 * x[1] + p[1] * jnp.sin(x[1] + x[3]) + 0.3 * u[1]
     dx2_dt = -0.6 * x[2] + jnp.cos(x[2] + x[0]) - 0.2 * u[2] ** 2
-    dx3_dt = -0.9 * x[3] + 0.01 * jnp.sin(x[3] + x[1])*jnp.cos(x[2] + x[0]) - 0.2 * u[0]
+    dx3_dt = -0.9 * x[3] + 0.01 * jnp.sin(x[3] + x[1]) * jnp.cos(x[2] + x[0]) - 0.2 * u[0]
 
     dx_dt = dx_dt.at[0].set(dx0_dt)
     dx_dt = dx_dt.at[1].set(dx1_dt)
@@ -21,6 +23,25 @@ def ode_fcn(x: jnp.ndarray = None, p: jnp.ndarray = None, t: jnp.ndarray = None,
     dx_dt = dx_dt.at[3].set(dx3_dt)
 
     return dx_dt
+
+n_par = 2
+chains = 100
+L = 500
+par = jax.random.uniform(key=jax.random.PRNGKey(7), minval=-4, maxval=4, shape=(n_par, chains), dtype=jnp.float64)
+x_0 = jax.random.uniform(key=jax.random.PRNGKey(7), minval=-4, maxval=4, shape=(4, chains), dtype=jnp.float64)
+u = jax.random.uniform(key=jax.random.PRNGKey(7), minval=-4, maxval=4, shape=(2, L), dtype=jnp.float64)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -40,6 +61,3 @@ def ode_fcn(x: jnp.ndarray = None, p: jnp.ndarray = None, t: jnp.ndarray = None,
 #
 # data = pd.read_csv('winequality-white.csv', delimiter=';')
 # data = jnp.array(data.values[:, :-2])
-
-
-
