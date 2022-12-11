d59d99cbc662c0fe826bd2f7c24b9bf8221d8b78
reza <maskari@hawk.iit.edu>
2022-12-11 11:43:43 -0600

    adding ode solvers

--- man_mcmc_simulation.py
+++ man_mcmc_simulation.py
@@ -5,6 +5,26 @@ import jax.numpy as jnp
 import optax
 from sklearn.decomposition import FactorAnalysis
 import pandas as pd
+from Ordinary_Differential_Equation_Solvers import ODESolvers
+
+def ode_fcn(x: jnp.ndarray = None, p: jnp.ndarray = None, t: jnp.ndarray = None, u: jnp.ndarray = None) -> jnp.ndarray:
+    m = 4  # the number of state variables
+    dx_dt = jnp.zeros((m, 1))  # reallocating the values of state variables
+    dx0_dt = -0.2 * x[0] + x[1] * x[2] + 0.1 * u[2]
+    dx1_dt = -0.1 * x[1] + jnp.sin(x[1] + x[3]) + 0.3 * u[1]
+    dx2_dt = -0.6 * x[2] + jnp.cos(x[2] + x[0]) - 0.2 * u[2] ** 2
+    dx3_dt = -0.9 * x[3] + 0.01 * jnp.sin(x[3] + x[1])*jnp.cos(x[2] + x[0]) - 0.2 * u[0]
+
+    dx_dt = dx_dt.at[0, 0].set(dx0_dt)
+    dx_dt = dx_dt.at[1, 0].set(dx1_dt)
+    dx_dt = dx_dt.at[2, 0].set(dx2_dt)
+    dx_dt = dx_dt.at[3, 0].set(dx3_dt)
+
+    return dx_dt
+
+
+
+
 
 # from Probablity_distributions import *
 # from tensorflow_probability.substrates.jax import distributions
