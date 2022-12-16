03b31b3332de7a394abcdacc83c71aab3a27c5bd
reza <maskari@hawk.iit.edu>
2022-12-15 20:36:38 -0600

    adding ode solvers

--- Feature_Selection.py
+++ Feature_Selection.py
@@ -28,68 +28,35 @@ class Fscchi2:
             self.y_n_categories = self.unique_y.shape[0]
         self.samples, self.n_predictors = self.x.shape
 
+        self.chi_squared_statistics = jnp.zeros((self.n_predictors,))
+        self.chi_squared_p_values = jnp.zeros((self.n_predictors,))
+        self.cramer_v = jnp.zeros((self.n_predictors,))
+
     def run(self):
 
         def inner_loop(m, values3):
-            unqqx, j, i, mat = values3
-            ff = jnp.where(self.x[:, j] == unqqx[m] & self.y == self.unique_y[i])
+            unqqx, feat_cnt, i, mat = values3
+            ff = jnp.where((self.x[:, feat_cnt] == unqqx[m]) & (self.y == self.unique_y[i]))[0]
             mat = mat.at[m, i].set(ff.shape[0])
-            return unqqx, j, i, mat
+            return unqqx, feat_cnt, i, mat
+
         def over_y_categories(i, values2):
-            mat, j, unqx, x_n = values2
+            mat, feat_cnt, unqx, x_n = values2
             for m in range(x_n):
-                unqx, j, i, mat=inner_loop(m=m, values3=(unqx, j, i, mat))
-            return mat, j, unqx, x_n
-        def over_features(j, values1):
-            unique_x = jnp.unique(self.x[:, j])
+                unqx, feat_cnt, i, mat = inner_loop(m=m, values3=(unqx, feat_cnt, i, mat))
+            return mat, feat_cnt, unqx, x_n
+
+        def over_features(feat_cnt, values1):
+            unique_x = jnp.unique(self.x[:, feat_cnt])
             x_n_categories = unique_x.shape[0]
             contigency_matrix = jnp.zeros((x_n_categories, self.y_n_categories))  # rows -> x categ, column ->y categ
             for i in range(self.y_n_categories):
-                over_y_categories(i=i, val=(contigency_matrix, j, unique_x, x_n_categories))
+                mat, feat_cnt, unqx, x_n = over_y_categories(i=i, values2=(
+                contigency_matrix, feat_cnt, unique_x, x_n_categories))
             return
 
-        for j in range(self.n_predictors):
-            over_features(j=j, values1=())
-
-        # def _sd(j: int, vc: tuple):
-        #     mat, i, unqy, xx = vc
-        #     c = jnp.where(xx == unqy)[0]
-        #     mat = mat.at[j, i].set(c)
-        #     return mat, i, unqy, xx
-        #
-        # def _chisquared_lone(i: int, val: tuple):
-        #     matrix, itr, unique_x, x_n = val
-        #     unq_y = self.unique_y[i]
-        #     # jax.lax.fori_loop(lower=0, upper=x_n, body_fun=_sd, init_val=(matrix, i, unq_y, self.x[:, itr]))
-        #     for j in range(x_n):
-        #         mat, i, unqy, xx = _sd(j=j, vc=(matrix,i,unq_y,self.x[:, itr]))
-        #
-        #     return
-        #
-        # def _chisquared_all(itr: int, values: tuple):
-        #     unique_x = jnp.unique(self.x[:, itr])
-        #     x_n_categories = unique_x.shape[0]
-        #     contigency_matrix = jnp.zeros((x_n_categories, self.y_n_categories))  # rows -> x categ, column ->y categ
-        #     for i in range(self.y_n_categories):
-        #         _chisquared_lone(i=i, val=(contigency_matrix, itr, unique_x, x_n_categories))
-
-        # jax.lax.fori_loop(lower=0,
-        #                   upper=self.y_n_categories,
-        #                   body_fun=_chisquared_lone,
-        #                   init_val=(contigency_matrix, itr, unique_x, x_n_categories))
-
-        # cTotal = obsCount.sum(axis=1)
-        # rTotal = obsCount.sum(axis=0)
-
-        #     return
-        #
-        # for itr in range(self.n_predictors):
-        #     vl = _chisquared_all(itr, values=None)
-
-        # jax.lax.fori_loop(lower=0,
-        #                   upper=self.n_predictors,
-        #                   body_fun=_chisquared_all,
-        #                   init_val=None)
+        for feat_cnt in range(self.n_predictors):
+            over_features(feat_cnt=feat_cnt, values_over_features=())
 
 
 data = pd.read_csv('winequality-white.csv', delimiter=';')
