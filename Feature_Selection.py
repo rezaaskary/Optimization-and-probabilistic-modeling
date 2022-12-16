aa99b3824985f34e45b4583a08d8d8160d217fa3
reza <maskari@hawk.iit.edu>
2022-12-15 22:14:53 -0600

    adding ode solvers

--- Feature_Selection.py
+++ Feature_Selection.py
@@ -39,38 +39,59 @@ class Fscchi2:
         def inner_loop(x_cat_cnt, values_inner_loop):
             unique_x, feat_cnt, y_cat_cnt, contigency_matrix = values_inner_loop
             counts = jnp.where((self.x[:, feat_cnt] == unique_x[x_cat_cnt]) & (self.y == self.unique_y[y_cat_cnt]))[0]
-            contigency_matrix = contigency_matrix.at[x_cat_cnt, y_cat_cnt].set(counts.shape[0])
+            contigency_matrix = contigency_matrix.at[x_cat_cnt, y_cat_cnt, feat_cnt].set(counts.shape[0])
             return unique_x, feat_cnt, y_cat_cnt, contigency_matrix
 
         def over_y_categories(y_cat_cnt, values_over_y_catg):
             contigency_matrix, feat_cnt, unique_x, x_n_ = values_over_y_catg
-            for x_cat_cnt in range(x_n_):
-                unique_x, feat_cnt, y_cat_cnt, contigency_matrix = inner_loop(x_cat_cnt=x_cat_cnt,
-                                                                              values_inner_loop=(unique_x,
-                                                                                                 feat_cnt,
-                                                                                                 y_cat_cnt,
-                                                                                                 contigency_matrix))
+            unique_x, feat_cnt, y_cat_cnt, contigency_matrix = jax.lax.fori_loop(lower=0,
+                                                                                 upper=x_n_,
+                                                                                 body_fun=inner_loop,
+                                                                                 init_val=(unique_x,
+                                                                                           feat_cnt,
+                                                                                           y_cat_cnt,
+                                                                                           contigency_matrix))
+
+
+            # for x_cat_cnt in range(x_n_):
+            #     unique_x, feat_cnt, y_cat_cnt, contigency_matrix = inner_loop(x_cat_cnt=x_cat_cnt,
+            #                                                                   values_inner_loop=(unique_x,
+            #                                                                                      feat_cnt,
+            #                                                                                      y_cat_cnt,
+            #                                                                                      contigency_matrix))
             return contigency_matrix, feat_cnt, unique_x, x_n_
 
         def over_features(feat_cnt, values_over_features):
-            contigency_matrix = values_over_features
-
+            contigency_matrix, unique_x_count = values_over_features
             unique_x = jnp.unique(self.x[:, feat_cnt])
             x_n_ = unique_x.shape[0]
+            unique_x_count = unique_x_count.at[feat_cnt].set(x_n_)
+            contigency_matrix, feat_cnt, unique_x, x_n_ = jax.lax.fori_loop(lower=0,
+                                                                            upper=self.y_n_,
+                                                                            body_fun=over_y_categories,
+                                                                            init_val=(contigency_matrix,
+                                                                                      feat_cnt,
+                                                                                      unique_x,
+                                                                                      x_n_))
 
-            for y_cat_cnt in range(self.y_n_):
-                contigency_matrix, feat_cnt, unique_x, x_n_ = over_y_categories(y_cat_cnt=y_cat_cnt,
-                                                                                values_over_y_catg=(contigency_matrix,
-                                                                                                    feat_cnt,
-                                                                                                    unique_x,
-                                                                                                    x_n_))
+            # for y_cat_cnt in range(self.y_n_):
+            #     contigency_matrix, feat_cnt, unique_x, x_n_ = over_y_categories(y_cat_cnt=y_cat_cnt,
+            #                                                                     values_over_y_catg=(contigency_matrix,
+            #                                                                                         feat_cnt,
+            #                                                                                         unique_x,
+            #                                                                                         x_n_))
+            return contigency_matrix, unique_x_count
 
-            ####
-            return contigency_matrix
+        contigency_matrix, unique_x_count = jax.lax.fori_loop(lower=0,
+                                                              upper=self.n_predictors,
+                                                              body_fun=over_features,
+                                                              init_val=(self.contigency_matrix.copy(),
+                                                                        self.unique_x_count.copy()))
 
-        for feat_cnt in range(self.n_predictors):
-            chi_squared_statistics, chi_squared_p_values, cramer_v = over_features(feat_cnt=feat_cnt,
-                                                                                   values_over_features=self.contigency_matrix.copy(), )
+        # for feat_cnt in range(self.n_predictors):
+        #     contigency_matrix, unique_x_count = over_features(feat_cnt=feat_cnt, values_over_features=
+        #     (self.contigency_matrix.copy(),
+        #      self.unique_x_count.copy()))
 
 
 data = pd.read_csv('winequality-white.csv', delimiter=';')
@@ -80,13 +101,13 @@ y_0 = jnp.round(x_0[:, 7])
 
 import numpy as np
 
-obsCount = pd.crosstab(index=x_0[:, 1], columns=y_0, margins=False, dropna=True)
-cTotal = obsCount.sum(axis=1)
-rTotal = obsCount.sum(axis=0)
-nTotal = np.sum(rTotal)
-expCount = np.outer(cTotal, (rTotal / nTotal))
-chiSqStat = ((obsCount - expCount) ** 2 / expCount).to_numpy().sum()
-chiSqDf = (obsCount.shape[0] - 1.0) * (obsCount.shape[1] - 1.0)
-chiSqSig = scipy.stats.chi2.sf(chiSqStat, chiSqDf)
+# obsCount = pd.crosstab(index=x_0[:, 1], columns=y_0, margins=False, dropna=True)
+# cTotal = obsCount.sum(axis=1)
+# rTotal = obsCount.sum(axis=0)
+# nTotal = np.sum(rTotal)
+# expCount = np.outer(cTotal, (rTotal / nTotal))
+# chiSqStat = ((obsCount - expCount) ** 2 / expCount).to_numpy().sum()
+# chiSqDf = (obsCount.shape[0] - 1.0) * (obsCount.shape[1] - 1.0)
+# chiSqSig = scipy.stats.chi2.sf(chiSqStat, chiSqDf)
 
 DD = Fscchi2(x=x_0, y=y_0).run()
