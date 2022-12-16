c1868599f91f9064b7d0bbcae9c2b17cd26c9eb9
reza <maskari@hawk.iit.edu>
2022-12-15 20:55:35 -0600

    adding ode solvers

--- Feature_Selection.py
+++ Feature_Selection.py
@@ -25,50 +25,52 @@ class Fscchi2:
         if self.unique_y.shape[0] < 2:
             raise Exception('The target variable is made of only one categorical variable.')
         else:
-            self.y_n_categories = self.unique_y.shape[0]
+            self.y_n_ = self.unique_y.shape[0]
         self.samples, self.n_predictors = self.x.shape
-
         self.chi_squared_statistics = jnp.zeros((self.n_predictors,))
         self.chi_squared_p_values = jnp.zeros((self.n_predictors,))
         self.cramer_v = jnp.zeros((self.n_predictors,))
 
     def run(self):
 
-        def inner_loop(m, values3):
-            unqqx, feat_cnt, i, mat = values3
-            ff = jnp.where((self.x[:, feat_cnt] == unqqx[m]) & (self.y == self.unique_y[i]))[0]
-            mat = mat.at[m, i].set(ff.shape[0])
-            return unqqx, feat_cnt, i, mat
+        def inner_loop(x_cat_cnt, values3):
+            unique_x, feat_cnt, y_cat_cnt, mat = values3
+            ff = jnp.where((self.x[:, feat_cnt] == unique_x[x_cat_cnt]) & (self.y == self.unique_y[y_cat_cnt]))[0]
+            mat = mat.at[x_cat_cnt, y_cat_cnt].set(ff.shape[0])
+            return unique_x, feat_cnt, y_cat_cnt, mat
 
-        def over_y_categories(i, values2):
-            mat, feat_cnt, unqx, x_n = values2
-            for m in range(x_n):
-                unqx, feat_cnt, i, mat = inner_loop(m=m, values3=(unqx, feat_cnt, i, mat))
-            return mat, feat_cnt, unqx, x_n
+        def over_y_categories(y_cat_cnt, values_over_y_catg):
+            contigency_matrix, feat_cnt, unique_x, x_n_ = values_over_y_catg
+            for x_cat_cnt in range(x_n_):
+                unique_x, feat_cnt, y_cat_cnt, contigency_matrix = inner_loop(x_cat_cnt=x_cat_cnt,
+                                                                              values3=(unique_x,
+                                                                                       feat_cnt,
+                                                                                       y_cat_cnt,
+                                                                                       contigency_matrix))
+            return contigency_matrix, feat_cnt, unique_x, x_n_
 
         def over_features(feat_cnt, values_over_features):
             chi_squared_statistics, chi_squared_p_values, cramer_v = values_over_features
 
-
             unique_x = jnp.unique(self.x[:, feat_cnt])
-            x_n_categories = unique_x.shape[0]
-            contigency_matrix = jnp.zeros((x_n_categories, self.y_n_categories))  # rows -> x categ, column ->y categ
-            for i in range(self.y_n_categories):
-                mat, feat_cnt, unqx, x_n = over_y_categories(i=i, values2=(
-                contigency_matrix, feat_cnt, unique_x, x_n_categories))
+            x_n_ = unique_x.shape[0]
+            contigency_matrix = jnp.zeros((x_n_, self.y_n_))  # rows -> x categ, column ->y categ
+            for y_cat_cnt in range(self.y_n_):
+                contigency_matrix, feat_cnt, unique_x, x_n_ = over_y_categories(y_cat_cnt=y_cat_cnt,
+                                                                                values_over_y_catg=(contigency_matrix,
+                                                                                                    feat_cnt,
+                                                                                                    unique_x,
+                                                                                                    x_n_))
+
+            ####
             return chi_squared_statistics, chi_squared_p_values, cramer_v
-
-        self.chi_squared_statistics = jnp.zeros((self.n_predictors,))
-        self.chi_squared_p_values = jnp.zeros((self.n_predictors,))
-        self.cramer_v = jnp.zeros((self.n_predictors,))
-
 
         for feat_cnt in range(self.n_predictors):
             chi_squared_statistics, chi_squared_p_values, cramer_v = over_features(feat_cnt=feat_cnt,
                                                                                    values_over_features=(
-                                                                                    self.chi_squared_statistics.copy(),
-                                                                                    self.chi_squared_p_values.copy(),
-                                                                                    self.cramer_v.copy()))
+                                                                                       self.chi_squared_statistics.copy(),
+                                                                                       self.chi_squared_p_values.copy(),
+                                                                                       self.cramer_v.copy()))
 
 
 data = pd.read_csv('winequality-white.csv', delimiter=';')
