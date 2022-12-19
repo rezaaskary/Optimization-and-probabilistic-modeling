58976568c9dda66eda7e738f8ddf00a37febb43f
reza <maskari@hawk.iit.edu>
2022-12-18 19:47:23 -0600

    Sensitivity Analysis Module

--- Sensitivity_Analysis.py
+++ Sensitivity_Analysis.py
@@ -73,8 +73,12 @@ class FourierAmplitudeSensitivityTest:
             x_arg = x_arg.at[z_idx, j].set(g)
             return omega2, z_idx, x_arg, phi_arg
 
+        ff = lax.dynamic_slice(operand=idex_old, start_indices=(0,), slice_sizes=(0,))
+        ff
         def _phase_shift(i: int, values_1: tuple) -> tuple:
-            omega2, omega, idx_new, idex_old, z_idx, x_arg = values_1
+            omega2, omega, idx_new, idex_old, z_idx, x_arg, cntr = values_1
+            ff = lax.dynamic_slice(operand=idex_old, start_indices=(0,), slice_sizes=(cntr,))
+            # idx_new = lax.dynamic_update_slice(operand=idx_new, update=)
             idx_new = idx_new.at[0:i].set(idex_old[0:i])
             omega2 = omega2.at[idx_new].set(omega[1:])
             z_idx = z_idx.at[:].set(z_idx + i * self.n)
@@ -84,8 +88,7 @@ class FourierAmplitudeSensitivityTest:
                                                           init_val=(omega2, z_idx, x_arg, phi))
             # for j in range(self.num_vars):
             #     omega2, z_idx, x_arg, phi_arg = _phase_shift_inner(j=j, values_2=(omega2, z_idx, x_arg, phi))
-
-            return omega2, omega, idx_new, idex_old, z_idx, x_arg
+            return omega2, omega, idx_new, idex_old, z_idx, x_arg, cntr + 1
 
         # for i in range(self.num_vars):
         #     self.omega2, self.omega2, idx_new, idex_old, z, self.x = _phase_shift(i=i,
@@ -94,7 +97,7 @@ class FourierAmplitudeSensitivityTest:
         #                                                                               idex_old, z, self.x))
         self.omega2, self.omega2, idx_new, idex_old, z, \
             self.x = lax.fori_loop(lower=0, upper=self.num_vars, body_fun=_phase_shift,
-                                   init_val=(self.omega2, self.omega, idx_new, idex_old, z, self.x))
+                                   init_val=(self.omega2, self.omega, idx_new, idex_old, z, self.x, 0))
 
 
 problem = {
--- testing_II.py
+++ testing_II.py
@@ -3,6 +3,22 @@ import numpy as np
 import sklearn
 from sklearn import decomposition
 
+import jax.numpy as jnp
+from jax import lax
+import jax
+from functools import partial
+
+idx_new = jnp.arange(start=1, stop=10, dtype=jnp.int32)
+idex_old = jnp.arange(start=0, stop=9, dtype=jnp.int32)
+
+def dummy_df(i: int, values: tuple) -> tuple:
+    idex_old, idx_new = values
+    slice = lax.dynamic_slice(operand=idex_old, start_indices=(0,), slice_sizes=(i,))
+    idx_new = lax.dynamic_update_slice(operand=idx_new, update=slice, start_indices=(0,))
+    return idex_old, idx_new
+
+
+lax.fori_loop(lower=0, upper=9, body_fun=dummy_df, init_val=(idex_old, idx_new))
 
 data = pd.read_csv('winequality-white.csv', delimiter=';')
 data = np.array(data.values[:, :-2])
@@ -12,13 +28,13 @@ data = np.array(data.values[:, :-2])
 
 # data = ((data - data.mean(axis=0))/data.std(axis=0))
 
-pp = decomposition.FactorAnalysis(n_components=2,max_iter=10000,tol=1e-3,svd_method='lapack')
+pp = decomposition.FactorAnalysis(n_components=2, max_iter=10000, tol=1e-3, svd_method='lapack')
 DD1 = pp.fit(data)
 DD = pp.transform(data)
 # ee =DD.get_covariance()
-ew = DD1.components_.T@DD1.components_ +np.diag(DD1.noise_variance_)
+ew = DD1.components_.T @ DD1.components_ + np.diag(DD1.noise_variance_)
 new_data = (data - data.mean(axis=0)).T
 f = DD1.components_.T
-CC= np.linalg.inv(f.T@f + np.diag(DD1.noise_variance_))@f.T@new_data
+CC = np.linalg.inv(f.T @ f + np.diag(DD1.noise_variance_)) @ f.T @ new_data
 
 CC
