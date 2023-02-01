d0d8dffc41116aed967d841043d821dab63a3422
rezaaskary <reza.askary99@gmail.com>
2023-02-01 13:30:11 -0600

    main_wrppring with docker

--- testing_II.py
+++ testing_II.py
@@ -7,11 +7,13 @@ import jax.numpy as jnp
 from jax import lax
 
 idx_new = jnp.arange(start=1, stop=10, dtype=jnp.int32)
+
+
 # idex_old = jnp.arange(start=0, stop=9, dtype=jnp.int32)
 
 
 def body_fun(i: int, idx_new):
-    idx_new = jnp.where(i==0, idx_new, idx_new.at[i-1].set(idx_new[i-1] - 1))
+    idx_new = jnp.where(i == 0, idx_new, idx_new.at[i - 1].set(idx_new[i - 1] - 1))
     # idx_new = idx_new.at[i].set(idx_new[i] - 1)
     # idex_old, idx_new = values
     # slice = lax.dynamic_slice(operand=idex_old, start_indices=(0,), slice_sizes=(i,))
